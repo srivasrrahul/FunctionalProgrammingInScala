845fbea5cc99b02ca9cc36a1a2770a4c71ba190e
srivasrrahul <srivasrrahul@gmail.com>
2020-04-08 11:59:20 +0530

    Least Recently Used

--- src/algorithms/algorithms_2020/LeastRecentlyUsed.scala
+++ src/algorithms/algorithms_2020/LeastRecentlyUsed.scala
@@ -22,12 +22,19 @@ class LRUCache(_capacity: Int) {
   def put(key: Int, value: Int): Unit =  {
     //println("Key inserted " + key + " Size is " + linkedHashMap.size)
     if (linkedHashMap.size < capacity) {
+      linkedHashMap.remove(key) //
       linkedHashMap += ((key,value))
     }else {
       //println("Adding key " + key)
       //println("removed key " + linkedHashMap.head._1)
-      linkedHashMap.remove(linkedHashMap.head._1)
-      linkedHashMap += ((key,value))
+      if (linkedHashMap.contains(key)) {
+        linkedHashMap.remove(key)
+        linkedHashMap.addOne((key,value))
+      }else {
+        linkedHashMap.remove(linkedHashMap.head._1)
+
+        linkedHashMap += ((key, value))
+      }
     }
   }
 
@@ -36,7 +43,7 @@ class LRUCache(_capacity: Int) {
 }
 
 object Solution {
-  def main(args: Array[String]): Unit = {
+  def testCase1() : Unit = {
     val lruCache = new LRUCache(2)
     lruCache.put(1,1)
     lruCache.put(2,2)
@@ -47,6 +54,18 @@ object Solution {
     println(lruCache.get(1))
     println(lruCache.get(3))
     println(lruCache.get(4))
+  }
+
+  def testCase2() : Unit = {
+    val lruCache = new LRUCache(2)
+    lruCache.put(2,6)
+    lruCache.put(1,5)
+    lruCache.put(1,2)
+    println(lruCache.get(1))
+    println(lruCache.get(2))
+  }
+  def main(args: Array[String]): Unit = {
+    testCase2()
 
   }
 }
