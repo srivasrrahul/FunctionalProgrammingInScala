42acc8e4655bf562c04069ff1da277d78d2b45e0
srivasrrahul <srivasrrahul@gmail.com>
2020-04-16 19:32:22 +0530

    MinMeetingRooms.scala

---
+++ src/algorithms/algorithms_2020/MinMeetingRooms.scala
@@ -0,0 +1,55 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import util.control.Breaks._
+
+object Solution {
+
+  def minMeetingRooms(intervals: Array[Array[Int]]): Int = {
+    intervals.length match {
+      case 0 => 0
+      case _ => {
+        val sortedIntervals = intervals.sortWith(_(0) < _(0))
+        //val sortedIntervals = intervals
+
+
+        def ifOverlap(i1 : Array[Int],i2 : Array[Int]) : Boolean = {
+          if (i1(1) > i2(0)) {
+            true
+          }else {
+            false
+          }
+        }
+
+        var maxDepth = 1
+
+        for (j <- 1 to sortedIntervals.length-1) {
+          //println("Current = " + sortedIntervals(j).mkString(","))
+          //println(stack.mkString(","))
+          var matchCount = 1 //at least with self
+          for (k <- j-1 to 0 by -1) {
+            if (ifOverlap(sortedIntervals(k),sortedIntervals(j))) {
+              matchCount += 1
+            }
+          }
+
+          if (matchCount > maxDepth) {
+            maxDepth = matchCount
+          }
+        }
+
+        maxDepth
+      }
+    }
+
+
+
+
+
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(minMeetingRooms(Array(Array(3,11),Array(4,9),Array(4,17),Array(9,10))))
+
+  }
+}
