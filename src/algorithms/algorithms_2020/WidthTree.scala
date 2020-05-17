2642aea699a21a7835586e6e66545ec97ee258e2
srivasrrahul <srivasrrahul@gmail.com>
2020-05-17 09:14:10 +0530

    WidthTree.scala

---
+++ src/algorithms/algorithms_2020/WidthTree.scala
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+
+object Solution {
+  def widthOfBinaryTree(root: TreeNode): Int = {
+    val q = new scala.collection.mutable.Queue[TreeNode]
+    var maxWidth = Int.MinValue
+    if (root != null) {
+      q.addOne(root)
+      maxWidth = 1
+    }
+
+
+    while (q.isEmpty == false) {
+
+      var dequeuedLst = q.dequeueAll(_ => true)
+
+      //trip left and right nulls
+      val newLst = new scala.collection.mutable.ListBuffer[TreeNode]
+      var initEncountered = false
+
+      for (element <- dequeuedLst) {
+        if (element != null) {
+          if (element.left != null) {
+            newLst.append(element.left)
+            initEncountered = true
+          }else {
+            if (initEncountered == true) {
+              newLst.append(element.left)
+            }
+          }
+
+          if (element.right != null) {
+            newLst.append(element.right)
+            initEncountered = true
+          }else {
+            if (initEncountered == true) {
+              newLst.append(element.right)
+            }
+          }
+        }else {
+          if (initEncountered == true) {
+            newLst.append(null)
+            newLst.append(null)
+          }
+        }
+      }
+
+      //trim from right
+      var reverseLst = newLst.reverse
+      while (reverseLst.size > 0 && reverseLst.head == null) {
+        reverseLst = reverseLst.tail
+      }
+
+      if (reverseLst.size > maxWidth) {
+        maxWidth = reverseLst.size
+      }
+
+      q.addAll(reverseLst.reverse)
+
+    }
+
+    if (maxWidth == Int.MinValue) {
+      0
+    }else {
+      maxWidth
+    }
+
+  }
+}
