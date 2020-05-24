3c0154413275b4de579ce5bcd5b6ae1deb66b45e
srivasrrahul <srivasrrahul@gmail.com>
2020-05-24 09:59:09 +0530

    LCIS

--- src/algorithms/algorithms_2020/LongestContinuosIncreasingSubsequence.scala
+++ src/algorithms/algorithms_2020/LongestContinuosIncreasingSubsequence.scala
@@ -1,24 +1,26 @@
 object Solution {
   def findLengthOfLCIS(nums: Array[Int]): Int = {
     if (nums.length > 0) {
-      val lcis = new Array[Int](nums.length)
-      lcis(0) = 1
+      //val lcis = new Array[Int](nums.length)
+      var maxLcisLen = 1
+      var anchorLen = 1
       for (j <- 1 to nums.length - 1) {
-        if (nums(j) > nums(j - 1)) {
-          lcis(j) = lcis(j - 1) + 1
-        } else {
-          //start from current
-          lcis(j) = 1
+        if (nums(j) > nums(j-1)) {
+          anchorLen = anchorLen + 1
+        }else {
+          anchorLen = 1 //start from current
         }
+
+        maxLcisLen = scala.math.max(anchorLen,maxLcisLen)
       }
 
-      lcis.max
+      maxLcisLen
     }else {
       0
     }
   }
 
   def main(args: Array[String]): Unit = {
-    println(findLengthOfLCIS(Array(2,2,2,2,2)))
+    println(findLengthOfLCIS(Array(2,2,2,2)))
   }
 }
