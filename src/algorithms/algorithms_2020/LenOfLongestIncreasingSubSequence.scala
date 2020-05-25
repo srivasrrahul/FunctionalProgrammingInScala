7c9f91c4477aa64a51de6d70e71b5c956396c77f
srivasrrahul <srivasrrahul@gmail.com>
2020-05-25 11:03:02 +0530

    Len of Longest Increasing Subsequence

--- src/algorithms/algorithms_2020/LenOfLongestIncreasingSubSequence.scala
+++ src/algorithms/algorithms_2020/LenOfLongestIncreasingSubSequence.scala
@@ -1,71 +1,66 @@
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.util.control.Breaks._
 
 object Solution {
   def findNumberOfLIS(nums: Array[Int]): Int = {
     if (nums.length > 0) {
-      val largestLIS = new Array[List[List[Int]]](nums.length)
-      largestLIS(0) = List(List(nums(0)))
-      var globalMax = 1
-      for (j <- 1 to nums.length - 1) {
-        val solution = new ListBuffer[List[Int]]
-        var maxSizeTillNow = 0
-        //println(nums(j))
-        for (k <- j - 1 to 0 by -1) {
-          //println(j + " " + k)
-          val lsts = largestLIS(k)
-          for (lst <- lsts) {
-            if (lst.last < nums(j)) {
-              //println(lst.size+1)
-              if (lst.size + 1 >= maxSizeTillNow) {
-                solution.append((nums(j) :: lst.reverse).reverse)
-                maxSizeTillNow = lst.size + 1
-              }
-            }
+      val countLIS = new Array[(Int,Int)](nums.length) //len and its count
+      countLIS(0) = (1,1)
+
+      for (j <- 1 to nums.length-1) {
+        val countMap = new mutable.TreeMap[Int,Int]()(Ordering[Int].reverse)
+        for (k <- j-1 to 0 by -1) {
+          if (nums(j) > nums(k)) {
+            val localCountLIS = countLIS(k)._1 + 1
+            val defaultCount = countMap.getOrElseUpdate(localCountLIS,0)
+            countMap += ((localCountLIS,defaultCount+countLIS(k)._2))
           }
-
-        }
-        //start from here
-        if (maxSizeTillNow < 1) {
-          solution.append(List(nums(j)))
-          maxSizeTillNow = 1
         }
 
-        //filter which is not at least the size
-        solution.filterInPlace(lst => {
-          lst.size >= maxSizeTillNow
-        })
+        //println("For j " + j + " " + countMap)
 
-        if (maxSizeTillNow > globalMax) {
-          globalMax = maxSizeTillNow
-        }
+        val localCountOnlySelf = 1
+        val defaultCount = countMap.getOrElseUpdate(localCountOnlySelf,0)
+        countMap += ((localCountOnlySelf,defaultCount+1))
 
-
-        largestLIS(j) = solution.toList
+        countLIS(j) = (countMap.head._1,countMap.head._2)
       }
 
-      println(largestLIS.mkString("\n"))
-
-      //println("global max " + globalMax)
-
-      var count = 0
-      largestLIS.foreach(lsts => {
-        for (lst <- lsts) {
-          println(lst)
-          if (lst.size >= globalMax) {
-            count = count + 1
-          }
+      //println(countLIS.mkString(","))
+      val maxCount = countLIS.max(new Ordering[(Int,Int)] {
+        override def compare(x: (Int, Int), y: (Int, Int)): Int = {
+          x._1.compareTo(y._1)
         }
-      })
+      })._1
 
-      count
+      //println(maxCount)
+      var totalCount = 0
+      for ((len,count) <- countLIS) {
+        if (len >= maxCount) {
+          totalCount = totalCount + count
+        }
+      }
+
+      totalCount
+
+
+      //0
+
     }else {
       0
     }
   }
 
+
+
+  //1,2,4,5,7
+  //1,2,3,4,7
+  //1,2,3,5,7
   def main(args: Array[String]): Unit = {
     println(findNumberOfLIS(Array(1,3,5,4,7)))
+    println(findNumberOfLIS(Array(2,2,2,2,2)))
+    println(findNumberOfLIS(Array(1,2,4,3,5,4,7,2)))
   }
 
 }
