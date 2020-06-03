319dc6c2ce2e4bd27f4afe3292e78d31bbe51c2e
srivasrrahul <srivasrrahul@gmail.com>
2020-06-03 16:40:29 +0530

    One Edit Distance Optimal

--- src/algorithms/algorithms_2020/OneEditDistance.scala
+++ src/algorithms/algorithms_2020/OneEditDistance.scala
@@ -1,37 +1,93 @@
 object Solution {
   def isOneEditDistance(s: String, t: String): Boolean = {
     //Plain edit distance
-    val matrix = Array.ofDim[Int](s.length+1,t.length+1)
+    val diffLen = scala.math.abs(s.length - t.length)
 
-    for (j <- 0 to t.length) {
-      matrix(0)(j) = j
-    }
+    if (diffLen > 1) {
+      false
+    }else {
+      var possible = false
+      val tSet = t.toSet
+      if (s.length < t.length) {
+        //add a char from tSet at different location
+        for (tChar <- tSet if possible == false) {
+          for (j <- 0 to s.length if possible == false) {
+            val prefix = s.substring(0,j)
+            val newStr = tChar.toString
+            val suffix = s.substring(j)
+            //println(prefix + " " + newStr + " " + suffix)
+            if ((prefix ++ newStr ++ suffix) == t) {
+              possible = true
+            }
+          }
+        }
 
-    for (j <- 0 to s.length) {
-      matrix(j)(0) = j
-    }
-
-    for (j <- 1 to s.length) {
-      for (k <- 1 to t.length) {
-        val option1 = matrix(j-1)(k) + 1
-        val option2 = matrix(j)(k-1) + 1
-
-        val diff = if (s(j-1) == t(k-1)) 0 else 1
-        val option3 = matrix(j-1)(k-1) + diff
-        matrix(j)(k) = Array(option1,option2,option3).min
+        if (possible == false) {
+          if (s.length == 0) {
+            possible = true
+          }
+        }
       }
+
+
+
+      if (possible == false) {
+        if (s.length > t.length) {
+          //delete one char from s
+          //println("here")
+
+          for (j <- 0 to s.length-1 if possible == false) {
+            val stringBuilder = new StringBuilder(s)
+            //println(stringBuilder.deleteCharAt(j).toString())
+            if (stringBuilder.deleteCharAt(j).toString() == t) {
+              possible = true
+            }
+          }
+
+          if (possible == false) {
+            if (t.length == 0) {
+              possible = true
+            }
+          }
+        }
+
+
+      }
+
+      if (possible == false) {
+        if (s.length == t.length && s != t) {
+
+          //println("here")
+          var diffCount = 0
+          for (j <- 0 to s.length-1 if possible == false) {
+            if (s(j) != t(j)) {
+              diffCount = diffCount + 1
+            }
+//            for (tChar <- tSet if possible == false) {
+//              val stringBuilder = new StringBuilder(s)
+//              if (stringBuilder.setCharAt(j,tChar).toString() == t) {
+//                possible = true
+//              }
+//            }
+
+          }
+//
+          if (diffCount == 1) {
+            possible = true
+          }
+
+        }
+      }
+
+      possible
+
+
     }
-
-//    for (j <- 0 to matrix.length-1) {
-//      println(matrix(j).mkString(","))
-//    }
-
-    matrix(s.length)(t.length) == 1
   }
 
 
 
   def main(args: Array[String]): Unit = {
-    println(isOneEditDistance("abc","ab"))
+    println(isOneEditDistance("ab","ac"))
   }
 }
