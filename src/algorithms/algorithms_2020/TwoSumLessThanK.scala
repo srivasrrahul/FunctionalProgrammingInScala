941b771a9f208ad2727d9566210866ce01cc378b
srivasrrahul <srivasrrahul@gmail.com>
2020-06-05 15:54:32 +0530

    Two Sum less Two pointers

--- src/algorithms/algorithms_2020/TwoSumLessThanK.scala
+++ src/algorithms/algorithms_2020/TwoSumLessThanK.scala
@@ -2,24 +2,22 @@ import scala.collection.mutable
 
 object Solution {
   def twoSumLessThanK(A: Array[Int], K: Int): Int = {
-    val presence = new mutable.TreeSet[Int]()
-    var found = false
-    var retValue = -1
-    for (j <- 0 to A.length-1) {
-      val diff = K - A(j)
-      val subTree = presence.rangeUntil(diff)
-      //println(A(j) + " " + diff + " " + subTree)
-      if (subTree.size > 0) {
-        val localMaxValue = subTree.last + A(j)
-        if (localMaxValue > retValue) {
-          retValue = localMaxValue
-        }
-      }
+    A.sortInPlace()
+    var i = 0
+    var j = A.length-1
 
-      subTree.add(A(j))
+    var solution = -1
+
+    while (i < j) {
+      if (A(i) + A(j) >= K) {
+        j = j -1
+      }else {
+        solution = scala.math.max(solution,A(i) + A(j))
+        i = i + 1
+      }
     }
 
-    retValue
+    solution
   }
 
   def main(args: Array[String]): Unit = {
