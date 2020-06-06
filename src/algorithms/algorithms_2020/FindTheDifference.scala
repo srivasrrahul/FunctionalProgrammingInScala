c85e1c15207a12473190c13256e739e2b2b69e61
srivasrrahul <srivasrrahul@gmail.com>
2020-06-06 19:32:56 +0530

    Find Diff using map

--- src/algorithms/algorithms_2020/FindTheDifference.scala
+++ src/algorithms/algorithms_2020/FindTheDifference.scala
@@ -1,23 +1,36 @@
+import scala.collection.mutable
+
 object Solution {
   def findTheDifference(s: String, t: String): Char = {
-    val sortedS = s.toSeq.sorted.unwrap
-    val sortedT = t.toSeq.sorted.unwrap
-    var diffFound = false
-    var diff = '0'
-    // a h z
-    // a b h z
-    for (j <- 0 to sortedS.length-1 if diffFound == false) {
-      if (sortedS(j) != sortedT(j)) {
-        diffFound = true
-        diff = sortedT(j)
+    val charCountS = new mutable.HashMap[Char,Int]()
+    for (ch <- s) {
+      val defaultCount = charCountS.getOrElseUpdate(ch,0)
+      charCountS += ((ch,defaultCount+1))
+    }
+
+    val charCountT = new mutable.HashMap[Char,Int]()
+    for (ch1 <- t) {
+      val defaultCount = charCountT.getOrElseUpdate(ch1,0)
+      charCountT += ((ch1,defaultCount+1))
+    }
+
+    var found = false
+    var foundChar = '0'
+    for ((ch,chCount) <- charCountT if found == false) {
+      charCountS.get(ch) match {
+        case None => {
+          found = true
+          foundChar = ch
+        }
+        case Some(tCount) => {
+          if (tCount != chCount) {
+            found = true
+            foundChar = ch
+          }
+        }
       }
     }
 
-    if (diffFound == false) {
-      diffFound = true
-      diff = sortedT.last
-    }
-
-    diff
+    foundChar
   }
 }
