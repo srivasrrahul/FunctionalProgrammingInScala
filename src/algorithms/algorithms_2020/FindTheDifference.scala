aa217c937e3463ed6303f544aa2d5dc19026ab20
srivasrrahul <srivasrrahul@gmail.com>
2020-06-06 19:40:25 +0530

    Find The diff XOR

--- src/algorithms/algorithms_2020/FindTheDifference.scala
+++ src/algorithms/algorithms_2020/FindTheDifference.scala
@@ -2,35 +2,22 @@ import scala.collection.mutable
 
 object Solution {
   def findTheDifference(s: String, t: String): Char = {
-    val charCountS = new mutable.HashMap[Char,Int]()
+    var res = 0
     for (ch <- s) {
-      val defaultCount = charCountS.getOrElseUpdate(ch,0)
-      charCountS += ((ch,defaultCount+1))
+      res = res ^ ch.toInt
     }
 
-    val charCountT = new mutable.HashMap[Char,Int]()
-    for (ch1 <- t) {
-      val defaultCount = charCountT.getOrElseUpdate(ch1,0)
-      charCountT += ((ch1,defaultCount+1))
+    for (ch <- t) {
+      res = res ^ ch.toInt
     }
 
-    var found = false
-    var foundChar = '0'
-    for ((ch,chCount) <- charCountT if found == false) {
-      charCountS.get(ch) match {
-        case None => {
-          found = true
-          foundChar = ch
-        }
-        case Some(tCount) => {
-          if (tCount != chCount) {
-            found = true
-            foundChar = ch
-          }
-        }
-      }
-    }
+    //println(res)
+    res.toChar
 
-    foundChar
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(findTheDifference("abc","abcd"))
   }
 }
