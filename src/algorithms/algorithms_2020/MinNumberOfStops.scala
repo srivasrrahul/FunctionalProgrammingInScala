c1a5e432ed3c7ff96a838f2c8e7d0fa7ec83e15c
srivasrrahul <srivasrrahul@gmail.com>
2020-06-07 13:38:28 +0530

    Min Number of Stops

--- src/algorithms/algorithms_2020/MinNumberOfStops.scala
+++ src/algorithms/algorithms_2020/MinNumberOfStops.scala
@@ -17,20 +17,33 @@ object Solution {
       //initialize
       val fuelPending = startFuel - stations(0)(0)
       if (fuelPending >= 0) {
-        val map = new mutable.TreeMap[Int, Int]()
+        //val map = new mutable.TreeMap[Int, Int]()
         stationFuelIndex(0) = List((1, fuelPending))
       } else {
         stationFuelIndex(0) = List()
       }
 
       for (j <- 1 to stations.length - 1) {
-        val lst = new ListBuffer[(Int, Int)]
+        //val lst = new mutable.ArrayBuffer[(Int, Int)]
+        val lst = new mutable.HashMap[Int,Int]() //hops petrol Available
         for (k <- j - 1 to 0 by -1) {
           val kthList = stationFuelIndex(k)
           for ((stopsAtK, fuelInTankAtStationK) <- kthList) {
             val pendingFuel = (fuelInTankAtStationK + stations(k)(1)) - (stations(j)(0) - stations(k)(0))
             if (pendingFuel >= 0) {
-              lst.append((stopsAtK + 1, pendingFuel))
+              val newKey = stopsAtK+1
+              lst.get(newKey) match {
+                case None => {
+                  lst += ((newKey,pendingFuel))
+                }
+                case Some(oldPendingFuel) => {
+                  //Select option with largest pending Fuel
+                  if (pendingFuel > oldPendingFuel) {
+                    lst += ((newKey,pendingFuel))
+                  }
+                }
+              }
+              //lst.append((stopsAtK + 1, pendingFuel))
             }
           }
         }
@@ -38,15 +51,24 @@ object Solution {
         //base case for first stop
         val pendingFuel = startFuel - stations(j)(0)
         if (pendingFuel >= 0) {
-          lst.append((1, pendingFuel))
+          if (lst.contains(1)) {
+            val oldPendingFuel = lst.get(1).get
+            if (pendingFuel > oldPendingFuel) {
+              lst += ((1, pendingFuel))
+            }
+          }else {
+            lst += ((1, pendingFuel))
+          }
+
         }
+
 
         //println(lst)
 
         stationFuelIndex(j) = lst.toList
       }
 
-      //println(stationFuelIndex.mkString(","))
+      println(stationFuelIndex.mkString("\n"))
 
 
       var minCount = Int.MaxValue
@@ -80,9 +102,10 @@ object Solution {
 
   def main(args: Array[String]): Unit = {
     //val stations = Array(Array(10,100))
-    //val stations = Array(Array(10,60),Array(20,30),Array(30,30),Array(60,40))
-    val stations = Array(Array(5,100),Array(997,100),Array(998,100))
-    println(minRefuelStops(999,1000,stations))
+    val stations = Array(Array(10,60),Array(20,30),Array(30,30),Array(60,40))
+    //val stations = Array(Array(5,100),Array(997,100),Array(998,100))
+    //println(minRefuelStops(100,10,stations))
+    println(minRefuelStops(100,50,Array(Array(25,25),Array(50,50))))
 
     //val arrStr = "[[1,186],[145,161],[183,43],[235,196],[255,169],[263,200],[353,161],[384,190],[474,44],[486,43],[567,48],[568,96],[592,36],[634,181],[645,167],[646,69],[690,52],[732,28],[800,42],[857,55],[922,63],[960,141],[973,13],[977,112],[997,162]]"
   }
