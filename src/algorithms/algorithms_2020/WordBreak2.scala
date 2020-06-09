11dc3cebfaac5f30940dbdbb7c3d85172e0e4820
srivasrrahul <srivasrrahul@gmail.com>
2020-06-09 10:42:32 +0530

    Word Break2

---
+++ src/algorithms/algorithms_2020/WordBreak2.scala
@@ -0,0 +1,80 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object Solution {
+  def wordBreak(s: String, wordDict: List[String]): List[String] = {
+    val wordSet = wordDict.toSet
+    val matrix = Array.ofDim[String](s.length,s.length)
+
+    for (j <- 0 to matrix.length-1) {
+      for (k <- 0 to matrix(j).length-1) {
+        matrix(j)(k) = ""
+      }
+    }
+
+    //println("here")
+    //var endFound = false
+    for (j <- 0 to matrix.length-1) {
+      for (k <- j to matrix(j).length-1 ) {
+        val subString = s.substring(j,k+1)
+        if (wordSet.contains(subString)) {
+          //println(subString)
+          matrix(j)(k) = subString
+
+        }
+      }
+    }
+
+    def itr(currentRow : Int) : List[List[String]] = {
+      //println("Current row is " + currentRow)
+      if (currentRow >= matrix.length) {
+        List()
+      }else {
+        val lstBuffer = new ListBuffer[List[String]]
+        for (k <- 0 to  matrix(currentRow).length-1) {
+          val str = matrix(currentRow)(k)
+
+          if (str != "") {
+            //println("Str is " + str)
+            val pendingLsts = itr(k+1)
+            if (pendingLsts.isEmpty == false) {
+              for (pendingLst <- pendingLsts) {
+                lstBuffer.append((str :: pendingLst))
+              }
+            }else {
+              lstBuffer.append(List(str))
+            }
+
+
+          }
+        }
+
+        lstBuffer.toList
+      }
+    }
+
+
+    val results = itr(0)
+    val retValue = new ListBuffer[String]
+    for (res <- results) {
+      val totalLength = res.foldLeft(0)((acc,newVal) => acc + newVal.length)
+      if (totalLength == s.length) {
+        retValue.append(res.mkString(" "))
+      }
+
+    }
+    //println(retValue)
+    retValue.toList
+
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    //println(wordBreak("catsandog",List("cat", "cats", "and", "sand", "dog")))
+    //println(wordBreak("a",List("a")))
+    val str = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    val lst = List("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa")
+
+    println(wordBreak(str,lst))
+  }
+}
