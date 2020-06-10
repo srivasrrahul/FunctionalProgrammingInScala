12d6e110f1a176c6c170a54bfec73f3661fc5d48
srivasrrahul <srivasrrahul@gmail.com>
2020-06-10 16:39:26 +0530

    Set Mis match

--- src/algorithms/algorithms_2020/SetMistmatch.scala
+++ src/algorithms/algorithms_2020/SetMistmatch.scala
@@ -3,17 +3,19 @@ import scala.collection.mutable
 object Solution {
   def findErrorNums(nums: Array[Int]): Array[Int] = {
     val n = nums.length
-    val mapCount = new mutable.HashSet[Int]()
+    //val mapCount = new mutable.HashSet[Int]()
 
     //1,2,2,4 = 7
 
+    val bitSet = new mutable.BitSet()
     var repeatedNumber = 0
-    for (num <- nums if repeatedNumber == 0) {
-      if (mapCount.contains(num)) {
+    for (num <- nums ) {
+      if (bitSet.contains(num)) {
         repeatedNumber = num
       }else {
-        mapCount.add(num)
+        bitSet.add(num)
       }
+
     }
 
     val missingNumber = (n*(n+1))/2 - (nums.sum - repeatedNumber)
