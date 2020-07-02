4968fc3b17291f13630c800bb7dacf6efb1fcfe0
srivasrrahul <srivasrrahul@gmail.com>
2020-07-02 13:24:43 +0530

    Sliding Window

--- src/algorithms/algorithms_2020/SlidingWindowAverage.scala
+++ src/algorithms/algorithms_2020/SlidingWindowAverage.scala
@@ -4,16 +4,20 @@ class MovingAverage(_size: Int) {
 
   /** Initialize your data structure here. */
   val lst = new ListBuffer[Int]
+  var sum = 0
 
   def next(v: Int): Double = {
     if (lst.size < _size) {
+      sum = sum + v
       lst.append(v)
     }else {
+      sum = sum - lst.head
       lst.dropInPlace(1)
       lst.append(v)
+      sum = sum + v
     }
 
-    lst.sum.toDouble/lst.size.toDouble
+    sum.toDouble/lst.size.toDouble
   }
 
 }
