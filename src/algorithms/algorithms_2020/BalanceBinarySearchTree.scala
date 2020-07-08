1e6c672ae2fb979f5cf4391383b0f2d0d1ddd2fe
srivasrrahul <srivasrrahul@gmail.com>
2020-07-08 19:26:10 +0530

    Balance BinarySearch Tree

--- src/algorithms/algorithms_2020/BalanceBinarySearchTree.scala
+++ src/algorithms/algorithms_2020/BalanceBinarySearchTree.scala
@@ -6,51 +6,39 @@ class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null)
 
 object Solution {
   def balanceBST(root: TreeNode): TreeNode = {
-    def createTree(lst : List[Int]) : (TreeNode,Int) = {
+    def createTree(lst : List[Int]) : TreeNode = {
       if (lst.isEmpty) {
-        (null,0)
+        null
       }else {
         if (lst.size == 1) {
-          (new TreeNode(lst.head),1)
+          new TreeNode(lst.head)
         }else {
           //println("Creatinh tree " + lst)
           val (firstHalf,secondHalf) = lst.splitAt(lst.length/2)
 
-          val (leftNode,leftHeight) = createTree(firstHalf)
+          val leftNode = createTree(firstHalf)
           val topNode = new TreeNode(secondHalf.head)
-          val (rightNode,rightHeight) = createTree(secondHalf.tail)
+          val rightNode = createTree(secondHalf.tail)
 
           topNode.left = leftNode
           topNode.right = rightNode
-          (topNode,1+math.max(leftHeight,rightHeight))
+          topNode
         }
       }
 
     }
-    def balance(node : TreeNode) : (List[Int],(TreeNode,Int)) = {
+    def balance(node : TreeNode) : List[Int] = {
       if (node == null) {
-        (List(),(null,0))
+        (List())
       }else {
-        val (leftLst,(leftNode,leftSize)) = balance(node.left)
-        val (rightLst,(rightNode,rightSize)) = balance(node.right)
-        node.left = leftNode
-        node.right = rightNode
-
-        if (math.abs(leftSize-rightSize) <= 1) {
-          //println("ss")
-          ((leftLst ++ List(node.value) ++ rightLst),(node,math.max(leftSize,rightSize)+1))
-        }else {
-          //println("hello")
-          //balance it by creating a new treeNode and splitting to half
-          val combinedLst = leftLst ++ List(node.value) ++ rightLst
-          val (newNode,newHeight) = createTree(combinedLst)
-          (combinedLst,(newNode,newHeight))
-        }
+        val leftLst =  balance(node.left)
+        val rightLst = balance(node.right)
+        leftLst ++ List(node.value) ++ rightLst
       }
     }
 
-    val (_,(newRoot,_)) = balance(root)
-    newRoot
+    val lst = balance(root)
+    createTree(lst)
 
   }
 }
