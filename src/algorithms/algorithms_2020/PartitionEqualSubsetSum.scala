53bc9732e43e049f47e046d074d8cb88aa510c07
srivasrrahul <srivasrrahul@gmail.com>
2020-07-11 18:31:36 +0530

    Partition Equal Sub Set optimal

--- src/algorithms/algorithms_2020/PartitionEqualSubsetSum.scala
+++ src/algorithms/algorithms_2020/PartitionEqualSubsetSum.scala
@@ -1,20 +1,20 @@
 object Solution {
   def canPartition(nums: Array[Int]): Boolean = {
-    def itr(index : Int,set1 : List[Int],set2 : List[Int]) : Boolean = {
+    def itr(index : Int,set1 : Int,set2 : Int) : Boolean = {
       if (index == nums.length) {
         //println("Finished " + set1 + " " + set2)
 
-        set1.sum == set2.sum
+        set1 == set2
       }else {
         //two options
-        itr(index+1,nums(index)::set1,set2) || itr(index+1,set1,nums(index)::set2)
+        itr(index+1,nums(index)+set1,set2) || itr(index+1,set1,nums(index)+set2)
       }
     }
 
-    itr(0,List(),List())
+    itr(0,0,0)
   }
 
   def main(args: Array[String]): Unit = {
-    println(canPartition(Array(1, 2, 3, 5)))
+    println(canPartition(Array(1, 5,11,5)))
   }
 }
