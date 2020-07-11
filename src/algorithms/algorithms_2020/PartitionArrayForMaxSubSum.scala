306cda7d45e8671e9b8f24f01748a7e3d1f14eab
srivasrrahul <srivasrrahul@gmail.com>
2020-07-11 18:07:00 +0530

    Partition Array for max sum

--- src/algorithms/algorithms_2020/PartitionArrayForMaxSubSum.scala
+++ src/algorithms/algorithms_2020/PartitionArrayForMaxSubSum.scala
@@ -15,27 +15,28 @@ object Solution {
       pq.addOne(node)
     }
 
-    def findPossibleSets(source : Int,visited : Set[Int]) : List[List[Int]] = {
-      val lstBuffer = new ListBuffer[List[Int]]
-      def path(lst : List[Int]) : Unit = {
-        if (lst.size <= K) {
-          lstBuffer.append(lst)
+    def findPossibleSets(source : Int,visited : Set[Int]) : List[Range] = {
+      val lstBuffer = new ListBuffer[Range]
+      def path(range: Range) : Unit = {
+        //println(range)
+        if (range.size <= K) {
+          lstBuffer.append(range)
 
-          val first = lst.head
-          val last = lst.last
+          val first = range.head
+          val last = range.last
           if (first-1 >=0 && visited.contains(first-1) == false) {
-            path((first-1) :: lst)
+            path(Range(first-1,last+1))
           }
 
           if (last+1 < A.length && visited.contains(last+1) == false) {
-            path(lst ++ List(last+1))
+            path(Range(first,last+2))
           }
         }
 
 
       }
 
-      path(List(source))
+      path(Range(source,source+1))
 
       lstBuffer.toList
     }
@@ -57,20 +58,27 @@ object Solution {
           val paths = findPossibleSets(top.index,visited)
           //println(paths)
           for (path <- paths) {
-            val currentSum = top.indexVal*path.length
-            findSums(localPq.clone(),visited.union(path.toSet),priorSum+currentSum)
+            val currentSum = top.indexVal*path.size
+            val localSet = new mutable.HashSet[Int]()
+            for (x <- path) {
+              localSet.add(x)
+            }
+            localSet.addAll(visited)
+            findSums(localPq.clone(),localSet.toSet,priorSum+currentSum)
           }
         }
       }
     }
+    //println(A.length)
     findSums(pq.clone(),Set(),0)
 
-    //println(sumLst)
+
     maxSum
 
   }
 
   def main(args: Array[String]): Unit = {
     println(maxSumAfterPartitioning(Array(1,15,7,9,2,5,10),3))
+    //println(maxSumAfterPartitioning(Array(20779,436849,274670,543359,569973,280711,252931,424084,361618,430777,136519,749292,933277,477067,502755,695743,413274,168693,368216,677201,198089,927218,633399,427645,317246,403380,908594,854847,157024,719715,336407,933488,599856,948361,765131,335089,522119,403981,866323,519161,109154,349141,764950,558613,692211),26))
   }
 }
