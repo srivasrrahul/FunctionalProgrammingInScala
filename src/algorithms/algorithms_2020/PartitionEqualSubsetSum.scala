ae8ececbe860bbd2792d728cb67273b704aa160e
srivasrrahul <srivasrrahul@gmail.com>
2020-07-11 19:41:39 +0530

    Partition Equal Subset sum

--- src/algorithms/algorithms_2020/PartitionEqualSubsetSum.scala
+++ src/algorithms/algorithms_2020/PartitionEqualSubsetSum.scala
@@ -1,5 +1,9 @@
+import scala.collection.mutable
+
+case class Key(val index : Int,val s1 : Int,val s2 : Int)
 object Solution {
   def canPartition(nums: Array[Int]): Boolean = {
+    val cache = new mutable.HashMap[Key,Boolean]()
     def itr(index : Int,set1 : Int,set2 : Int) : Boolean = {
       if (index == nums.length) {
         //println("Finished " + set1 + " " + set2)
@@ -7,7 +11,16 @@ object Solution {
         set1 == set2
       }else {
         //two options
-        itr(index+1,nums(index)+set1,set2) || itr(index+1,set1,nums(index)+set2)
+        val key = new Key(index,set1,set2)
+        if (cache.contains(key)) {
+          //println("Cache hit")
+          cache.get(key).get
+        }else {
+          val retValue = itr(index+1,nums(index)+set1,set2) || itr(index+1,set1,nums(index)+set2)
+          cache += ((key,retValue))
+          retValue
+        }
+
       }
     }
 
