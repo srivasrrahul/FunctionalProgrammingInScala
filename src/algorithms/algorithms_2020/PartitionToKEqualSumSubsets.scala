cc89c8549eb20511d57afd0823681b1b6ccec823
srivasrrahul <srivasrrahul@gmail.com>
2020-07-12 08:30:35 +0530

    Partition To K

---
+++ src/algorithms/algorithms_2020/PartitionToKEqualSumSubsets.scala
@@ -0,0 +1,35 @@
+import scala.collection.mutable
+
+object Solution {
+  def canPartitionKSubsets(nums: Array[Int], k: Int): Boolean = {
+    val cache = new mutable.HashMap[Array[Int],Boolean]()
+    def itr(currenIndex : Int,lst : Array[Int]): Boolean = {
+      if (currenIndex == nums.length) {
+        //println(lst.mkString(","))
+        lst.toSet.size == 1
+      }else {
+        var retValue = false
+        for (j <- 0 to k-1 if retValue == false) {
+          val newLst = lst.clone()
+          newLst(j) = newLst(j) + nums(currenIndex)
+          val localRetValue = itr(currenIndex+1,newLst)
+          if (localRetValue == true) {
+            retValue = true
+          }
+        }
+
+        retValue
+      }
+    }
+
+    val arr = new Array[Int](k)
+    for (j <- 0 to arr.length-1) {
+      arr(j) = 0
+    }
+    itr(0,arr)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(canPartitionKSubsets(Array(4, 3, 2, 3, 5, 2, 1),4))
+  }
+}
