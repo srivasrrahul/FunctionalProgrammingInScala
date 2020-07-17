6eee9f5c1b31c59ae539cc7c551a374e66469766
srivasrrahul <srivasrrahul@gmail.com>
2020-07-17 16:22:50 +0530

    Post Order Traversal

--- src/algorithms/algorithms_2020/PostOrderTraversal.scala
+++ src/algorithms/algorithms_2020/PostOrderTraversal.scala
@@ -1,4 +1,5 @@
-
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
   var value: Int = _value
@@ -8,16 +9,52 @@ class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null)
 
 object Solution {
   def postorderTraversal(root: TreeNode): List[Int] = {
-    def postOrder(node : TreeNode) : List[Int] = {
-      if (node == null) {
-        List()
-      }else {
-        val left = postOrder(node.left)
-        val right = postOrder(node.right)
-        left ++ right ++ List(node.value)
-      }
+    val stack = new mutable.Stack[(TreeNode,Int)]() //INt ==> 0 then 1 if 1 then dont insert
+    val lstBuffer = new ListBuffer[Int]
+
+    if (root != null) {
+      stack.push((root,0)) //0 left is unfinished, 1 left is added but unfinihsed, 2 left is finished and added right
     }
 
-    postOrder(root)
+    var current = root
+    while (stack.isEmpty == false) {
+      val (top,state) = stack.pop()
+      state match {
+        case 0 => {
+          val left = top.left
+          if (left != null) {
+            stack.push((top,1))
+            stack.push((left,0))
+          }else {
+            //if left is null
+            val right = top.right
+            if (right != null) {
+              stack.push((top,2))
+              stack.push((right,0))
+            }else {
+              //its leaft
+              lstBuffer.append(top.value)
+            }
+          }
+        }
+        case 1 => {
+          //left is proessed
+          val right = top.right
+          if (right != null) {
+            stack.push((top,2))
+            stack.push((right,0))
+          }else {
+            lstBuffer.append(top.value)
+          }
+        }
+        case 2 => {
+          lstBuffer.append(top.value)
+        }
+      }
+
+
+    }
+
+    lstBuffer.toList
   }
 }
