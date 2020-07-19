bd667ccfd4db54c70500916d316e359ef93074f6
srivasrrahul <srivasrrahul@gmail.com>
2020-07-19 12:54:06 +0530

    Best Time To Buy And Sell Stock 4

--- src/algorithms/algorithms_2020/BestTimeToBuyAndSellStock4.scala
+++ src/algorithms/algorithms_2020/BestTimeToBuyAndSellStock4.scala
@@ -1,37 +1,57 @@
+import scala.collection.mutable
+
+case class Id(val dayIndex : Int,val hasStock : Boolean,val buyPrice : Int,val numberOfPendingTransactions : Int)
 object Solution {
   def maxProfit(k: Int, prices: Array[Int]): Int = {
-    def itr(dayIndex : Int,hasStock : Boolean,buyPrice : Int) : Int = {
-      if (dayIndex == prices.length-1) {
-        if (hasStock == true) {
-          if (prices.last > buyPrice) {
-            prices.last - buyPrice
+    val cache = new mutable.HashMap[Id,Int]()
+    def itr(dayIndex : Int,hasStock : Boolean,buyPrice : Int,numberOfTransactionsPending : Int) : Int = {
+      if (numberOfTransactionsPending <= 0) {
+        0
+      }else {
+        if (dayIndex == prices.length-1) {
+          //last day
+          if (hasStock == true) {
+            //println("for day " + dayIndex + " " + (prices.last - buyPrice))
+            math.max(prices.last - buyPrice,0)
           }else {
             0
           }
         }else {
-          0
-        }
-      }else {
-        //if you have stock
-        //two optiosn a) Sell it b) Wait
-        //if you don't have stcok
-        //two options a) Buy it b) Wait
-        if (hasStock == true) {
-          val opt1 = (prices(dayIndex) - buyPrice) + itr(dayIndex+1,false,0)
-          val opt2 = itr(dayIndex+1,hasStock,buyPrice)
-          scala.math.max(opt1,opt2)
-        }else {
-          val opt1 = itr(dayIndex+1,true,prices(dayIndex))
-          val opt2 = itr(dayIndex+1,false,0)
-          scala.math.max(opt1,opt2)
+          //if you have stock two options
+          //sell it or wait
+          //if you dont have two options
+          //buy it or wait
+          val id = new Id(dayIndex,hasStock,buyPrice,numberOfTransactionsPending)
+          if (cache.contains(id)) {
+            cache.get(id).get
+          }else {
+            if (hasStock == true) {
+              val opt1 = (prices(dayIndex) - buyPrice) + itr(dayIndex + 1, false, 0, numberOfTransactionsPending - 1)
+              val opt2 = itr(dayIndex + 1, hasStock, buyPrice, numberOfTransactionsPending)
+              //println("For day " + dayIndex + " " + hasStock + " " + buyPrice + " " + scala.math.max(opt1,opt2))
+              cache += ((id,scala.math.max(opt1, opt2)))
+              scala.math.max(opt1, opt2)
+            } else {
+              val opt1 = itr(dayIndex + 1, true, prices(dayIndex), numberOfTransactionsPending - 1)
+              val opt2 = itr(dayIndex + 1, hasStock, buyPrice, numberOfTransactionsPending)
+              //println("For day " + dayIndex + " " + hasStock + " " + buyPrice + " " + scala.math.max(opt1,opt2))
+              cache += ((id,scala.math.max(opt1, opt2)))
+              scala.math.max(opt1, opt2)
+            }
+          }
         }
       }
     }
 
-    itr(0,false,0)
+    if (prices.isEmpty == false) {
+      itr(0,false,0,2*k)
+    }else {
+      0
+    }
+
   }
 
   def main(args: Array[String]): Unit = {
-    println(maxProfit(2,Array(3,2,6,5,0,3)))
+    println(maxProfit(2,Array(3,3,5,0,0,3,1,4)))
   }
 }
