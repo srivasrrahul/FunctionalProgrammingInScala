b5a3137b9a645fb214c36a6b3d928ec1bec9515d
srivasrrahul <srivasrrahul@gmail.com>
2020-07-29 12:08:39 +0530

    Search Matrix 2D

--- src/algorithms/algorithms_2020/SearchMatrix2D.scala
+++ src/algorithms/algorithms_2020/SearchMatrix2D.scala
@@ -2,21 +2,28 @@ import scala.collection.Searching
 
 object Solution {
   def searchMatrix(matrix: Array[Array[Int]], target: Int): Boolean = {
-    def search(rowNum : Int) : Boolean = {
-      if (rowNum >= matrix.length) {
-        false
-      }else {
-        matrix(rowNum).search(target) match {
-          case Searching.Found(foundIndex) => {
-            true
+    if (matrix.isEmpty) {
+      false
+    }else {
+      val cols = matrix(0).length - 1
+
+      def search(rowBegin: Int, rowEnd: Int): Boolean = {
+        if (rowBegin == rowEnd) {
+          matrix(rowBegin).search(target) match {
+            case Searching.Found(_) => true
+            case _ => false
           }
-          case _ => {
-            search(rowNum+1)
+        } else {
+          val mid = rowBegin + (rowEnd - rowBegin) / 2
+          if (target <= matrix(mid)(cols)) {
+            search(rowBegin, mid)
+          } else {
+            search(mid + 1, rowEnd)
           }
         }
       }
-    }
 
-    search(0)
+      search(0, matrix.length - 1)
+    }
   }
 }
