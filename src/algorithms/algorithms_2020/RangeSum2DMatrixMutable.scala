dc5d75c7aef76dcef477d2251ccfa1b6e66f4131
srivasrrahul <srivasrrahul@gmail.com>
2020-07-30 12:46:26 +0530

    Range Sum 2D Matrix Mutable

---
+++ src/algorithms/algorithms_2020/RangeSum2DMatrixMutable.scala
@@ -0,0 +1,61 @@
+import scala.collection.mutable
+
+class NumMatrix(_matrix: Array[Array[Int]]) {
+  //sum of all rows and cols separately
+  val rowSum = new mutable.HashMap[Int,Array[Int]]()
+  for (j <- 0 to _matrix.length-1) {
+    val arr = new Array[Int](_matrix(0).length)
+    arr(0) = _matrix(j)(0)
+    for (k <- 1 to _matrix(0).length-1) {
+      arr(k) = arr(k-1) + _matrix(j)(k)
+    }
+
+    rowSum += ((j,arr))
+  }
+
+  // for ((k,v) <- rowSum) {
+  //     println("for k " + k + " " + v.mkString(","))
+  // }
+
+  def update(row: Int, col: Int, x: Int) : Unit = {
+    val arr = rowSum.get(row).get
+    //println(arr.mkString(","))
+
+    arr(col) = arr(col) + (x-_matrix(row)(col))
+    for (j <- col+1 to _matrix(0).length-1) {
+      arr(j) = arr(j-1) + _matrix(row)(j)
+    }
+
+    //rowSum += ((row,arr))
+    _matrix(row)(col) = x
+    //println(arr.mkString(","))
+  }
+  def sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int = {
+
+    var sum = 0
+    for (j <- row1 to row2) {
+      val currentSumArr = rowSum.get(j).get
+      var validSum = currentSumArr(col2)
+      if (col1 > 0) {
+        validSum = validSum - currentSumArr(col1-1)
+      }
+
+      sum = sum + validSum
+    }
+
+    sum
+  }
+
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * obj.update(row,col,`val`)
+ * var param_2 = obj.sumRegion(row1,col1,row2,col2)
+ */
