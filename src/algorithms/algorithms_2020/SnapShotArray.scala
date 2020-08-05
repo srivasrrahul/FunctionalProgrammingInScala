eae9f631c039781147452331333b07f4447404ec
srivasrrahul <srivasrrahul@gmail.com>
2020-08-05 10:29:43 +0530

    Snap Shot

--- src/algorithms/algorithms_2020/SnapShotArray.scala
+++ src/algorithms/algorithms_2020/SnapShotArray.scala
@@ -1,26 +1,57 @@
 import scala.collection.mutable
 
-case class VersionData(val arrIndex : Int,version : Int)
 class SnapshotArray(_length: Int) {
-  val data = new mutable.HashMap[Int,Array[Int]]()
-  val arr = new Array[Int](_length)
+  val mutatedDataFromLastVersion = new mutable.TreeMap[Int,mutable.HashMap[Int,Int]]()(Ordering[Int].reverse)
+  val latestCopy = new Array[Int](_length)
+  val baseVersion = new Array[Int](_length)
   var version = 0
-  def set(index: Int, v: Int) {
-    arr(index) = v
+  def set(index: Int, v: Int) : Unit = {
+    latestCopy(index) = v
+
+    if (version > 0) {
+      val changedInLatestMap = mutatedDataFromLastVersion.getOrElseUpdate(version,new mutable.HashMap[Int,Int]())
+      changedInLatestMap += ((index,v))
+    }
   }
 
   def snap(): Int = {
-    data += ((version,arr.clone()))
-    val oldVersion = version
-    version = version+1
-    oldVersion
+    if (version == 0) {
+      Array.copy(latestCopy,0,baseVersion,0,latestCopy.length)
+      version = 1
+      0
+    }else {
+      val oldVersion = version
+      version = version+1
+      oldVersion
+    }
   }
 
   def get(index: Int, snap_id: Int): Int = {
+    println(mutatedDataFromLastVersion)
     if (snap_id == version) {
-      arr(index)
+      latestCopy(index)
     }else {
-      data.get(snap_id).get(index)
+      //get latest less than or equal to version
+      val versionMap = mutatedDataFromLastVersion.rangeFrom(snap_id)
+      println("For index " + index + " " + versionMap)
+      if (versionMap.size > 0) {
+        var found = false
+        var retValue = -1
+        for ((versionId,dataChanged) <- versionMap if found == false) {
+          if (dataChanged.contains(index)) {
+            retValue = dataChanged.get(index).get
+            found = true
+          }
+        }
+
+        if (found == false) {
+          baseVersion(index)
+        }else {
+          retValue
+        }
+      }else {
+        baseVersion(index)
+      }
     }
   }
 
