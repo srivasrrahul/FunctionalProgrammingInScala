d5f4e71419eeb192b698b44c935c74d67febd29a
srivasrrahul <srivasrrahul@gmail.com>
2020-08-21 18:07:11 +0530

    Max Score

--- src/algorithms/algorithms_2020/DynamicProgramming/MaxScore.scala
+++ src/algorithms/algorithms_2020/DynamicProgramming/MaxScore.scala
@@ -8,14 +8,16 @@ object Solution {
     if (K == cardPoints.length) {
       cardPoints.sum
     }else {
-      val matrix = Array.ofDim[Int](cardPoints.length,cardPoints.length,K+1)
+      var sourceCount = 1 //destCount is 2
+      var sourceMatrix = Array.ofDim[Int](cardPoints.length,cardPoints.length)
+      var destMatrix = Array.ofDim[Int](cardPoints.length,cardPoints.length)
       for (j <- 0 to cardPoints.length-1) {
-        matrix(j)(j)(1) = cardPoints(j)
+        sourceMatrix(j)(j) = cardPoints(j)
       }
 
       for (j <- 0 to cardPoints.length-1) {
         for (k <- j to cardPoints.length-1) {
-          cardPoints(j)(k)(1) = math.max(cardPoints(j),cardPoints(k))
+          sourceMatrix(j)(k) = math.max(cardPoints(j),cardPoints(k))
         }
       }
 
@@ -30,7 +32,7 @@ object Solution {
         var k = current
 
         while (j < cardPoints.length && k < cardPoints.length) {
-          matrix(j)(k)(pickupCurrent) = math.max(cardPoints(j) + matrix(j + 1)(k)(pickupCurrent - 1), cardPoints(k) + matrix(j)(k - 1)(pickupCurrent - 1))
+          destMatrix(j)(k) = math.max(cardPoints(j) + sourceMatrix(j + 1)(k), cardPoints(k) + sourceMatrix(j)(k - 1))
 
           j = j + 1
           k = k + 1
@@ -44,21 +46,25 @@ object Solution {
 
         pickupBase = pickupBase + 1
         pickupCurrent = pickupBase
+
+        val tempMatrix = sourceMatrix
+        sourceMatrix = destMatrix
+        destMatrix = tempMatrix
       }
 
 
-      for (p <- 1 to K) {
-        println("===============")
-        for (j <- 0 to cardPoints.length-1) {
-          for (k <- 0 to cardPoints.length-1) {
-            print(matrix(j)(k)(p) + ",")
-          }
+//      for (p <- 1 to K) {
+//        println("===============")
+//        for (j <- 0 to cardPoints.length-1) {
+//          for (k <- 0 to cardPoints.length-1) {
+//            print(destMatrix(j)(k) + ",")
+//          }
+//
+//          println()
+//        }
+//      }
 
-          println()
-        }
-      }
-
-      matrix(0)(cardPoints.length-1)(K)
+      destMatrix(0)(cardPoints.length-1)
 
     }
 
