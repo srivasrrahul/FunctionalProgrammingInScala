df1bbe5e92f6f644cf22662de11a5bafb963f5de
srivasrrahul <srivasrrahul@gmail.com>
2020-08-26 15:28:23 +0530

    RaceCar

---
+++ src/algorithms/algorithms_2020/MockInterviews/RaceCar.scala
@@ -0,0 +1,123 @@
+import scala.collection.mutable
+
+case class Vector(val position : Int,val speed : Int)
+trait Msg
+case object MsgA extends Msg
+case object MsgR extends Msg
+
+object MsgHandler {
+  def handle(vector: Vector,msg : Msg) : Vector = {
+    msg match {
+      case MsgA => {
+        val newPos = vector.position + vector.speed
+        val newSpeed = vector.speed*2
+        new Vector(newPos,newSpeed)
+      }
+      case MsgR => {
+        var newSpeed = 0
+        if (vector.speed > 0) {
+          newSpeed = -1
+        }else {
+          newSpeed = 1
+        }
+
+        new Vector(vector.position,newSpeed)
+      }
+    }
+  }
+}
+object Solution {
+  def racecar(target: Int): Int = {
+    val initialVector = new Vector(0, 1)
+    //var minDistance = Int.MaxValue
+
+    val pq = mutable.PriorityQueue.empty[(Vector, Int)](new Ordering[(Vector, Int)] {
+      override def compare(x: (Vector, Int), y: (Vector, Int)): Int = {
+        val d1 = math.abs(x._1.position)
+        val d2 = math.abs(y._1.position)
+        d2.compareTo(d1)
+      }
+    })
+
+    val visited = new mutable.HashSet[Vector]()
+
+    pq.addOne((initialVector, 0))
+    var goalFound = false
+
+    var foundTargetDistance = Int.MaxValue
+    while (pq.isEmpty == false && goalFound == false) {
+      val top = pq.dequeue()
+      visited.add(top._1)
+      if (top._1.position == target) {
+        foundTargetDistance = top._2
+        goalFound = true
+      } else {
+        val nextMsgA = MsgHandler.handle(top._1, MsgA)
+        val nextMsgR = MsgHandler.handle(top._1, MsgR)
+
+        if (visited.contains(nextMsgA) == false) {
+          pq.addOne((nextMsgA, top._2 + 1))
+        }
+
+        if (visited.contains(nextMsgR) == false) {
+          pq.addOne((nextMsgR, top._2 + 1))
+        }
+      }
+    }
+
+    //Goal found with some path
+    //Check whatever was pending in pq if that traverses to distance and found that
+
+    val pq1 = mutable.PriorityQueue.empty[(Vector, Int)](new Ordering[(Vector, Int)] {
+      override def compare(x: (Vector, Int), y: (Vector, Int)): Int = {
+        //get shortest distance
+        y._2.compareTo(x._2)
+      }
+    })
+
+    visited.clear()
+    pq.clear()
+    //Apply dijkstra
+
+    pq1.addOne((initialVector,0))
+    val upperBound = foundTargetDistance
+    foundTargetDistance = 0
+    val distanceMap = new mutable.HashMap[Vector,Int]()
+    while (pq1.isEmpty == false) {
+      val top = pq1.dequeue()
+
+      val newDistance = top._2+1
+
+      if (newDistance > upperBound) {
+        //forget it
+      } else {
+        val nextMsgA = MsgHandler.handle(top._1, MsgA)
+        val nextMsgR = MsgHandler.handle(top._1, MsgR)
+        val existingDistanceA = distanceMap.getOrElse(nextMsgA,Int.MaxValue)
+        if (newDistance < existingDistanceA) {
+          distanceMap += ((nextMsgA,newDistance))
+          pq1.addOne((nextMsgA, top._2 + 1))
+        }
+
+        val existingDistanceR = distanceMap.getOrElse(nextMsgR,Int.MaxValue)
+        if (newDistance < existingDistanceR) {
+          distanceMap += ((nextMsgR,newDistance))
+          pq1.addOne((nextMsgR, top._2 + 1))
+        }
+
+      }
+    }
+
+    var minDistance = Int.MaxValue
+    for ((vec,distance) <- distanceMap) {
+      if (vec.position == target) {
+        if (distance < minDistance) {
+          minDistance = distance
+        }
+      }
+    }
+
+    minDistance
+
+  }
+}
