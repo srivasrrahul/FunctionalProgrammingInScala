34cfa7bb73877a0d006d74d5a427cebaef6086b4
srivasrrahul <srivasrrahul@gmail.com>
2020-09-02 09:59:21 +0530

    Binary Tree Cameras

---
+++ src/algorithms/algorithms_2020/MockInterviews/BinaryTreeCameras.scala
@@ -0,0 +1,64 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
+
+case class Index(val code : Int,val parentWatched : Boolean,val mustAddRoot : Boolean)
+object Solution {
+  def minCameraCover(root: TreeNode): Int = {
+    val cache = new mutable.HashMap[Index,Int]()
+    def itr(node : TreeNode,parentWatched : Boolean,mustAddAtRoot : Boolean) : Int = {
+      if (node == null) {
+        0
+      }else {
+        val index = new Index(node.hashCode(),parentWatched,mustAddAtRoot)
+        if (cache.contains(index)) {
+          cache.get(index).get
+        }else {
+          val optionLst = new ListBuffer[Int]
+          if (parentWatched == true) {
+            optionLst.append(itr(node.left, false, false) + itr(node.right, false, false))
+            optionLst.append(1 + itr(node.left, true, false) + itr(node.right, true, false))
+          } else {
+            //Need to add one at root
+            if (mustAddAtRoot) {
+              optionLst.append(1 + itr(node.left, true, false) + itr(node.right, true, false))
+            } else {
+              //You can add at current
+
+              optionLst.append(1 + itr(node.left, true, false) + itr(node.right, true, false))
+
+              //You can ask left to add compulsiorily
+              if (node.left != null) {
+                optionLst.append(itr(node.left, false, true) + itr(node.right, false, false))
+              }
+
+              //You can ask right to add compulsiorily
+              if (node.right != null) {
+                optionLst.append(itr(node.left, false, false) + itr(node.right, false, true))
+              }
+
+
+            }
+
+          }
+
+          val minValue = optionLst.min
+          cache += ((index,optionLst.min))
+          //optionLst.min
+          minValue
+        }
+      }
+    }
+
+    val a = itr(root,false,false)
+    val b = itr(root,false,true)
+
+    math.min(a,b)
+
+  }
+}
