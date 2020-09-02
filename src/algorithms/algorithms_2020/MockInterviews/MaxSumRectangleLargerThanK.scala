381fef44f799954b503547806f3a9313868101b7
srivasrrahul <srivasrrahul@gmail.com>
2020-09-02 11:19:20 +0530

    Max Sum Rectangle Larger Than K

--- src/algorithms/algorithms_2020/MockInterviews/MaxSumRectangleLargerThanK.scala
+++ src/algorithms/algorithms_2020/MockInterviews/MaxSumRectangleLargerThanK.scala
@@ -27,36 +27,41 @@ object Solution {
       total
     }
 
-    val sums = Array.ofDim[Int](rows,rows,cols,cols)
+    //val sums = Array.ofDim[Int](rows,rows)
+    val sums = Array.ofDim[Int](cols,cols)
 
     //println(rowSums.get(0).get.mkString(","))
     var globalMax = Int.MinValue
-    for (j <- 0 to rows-1) {
-      for (k <- 0 to cols-1) {
-        for (p <- k to cols-1) {
-          sums(j)(j)(k)(p) = getRowSum(j,k,p)
-          val s = sums(j)(j)(k)(p)
-          //println(k + " " + p + " : " + s)
-          if (s <= K && s > globalMax) {
-            globalMax = s
-            //println("Global max " + globalMax)
-          }
-        }
-      }
-    }
+    //    for (j <- 0 to rows-1) {
+    //      for (k <- 0 to cols-1) {
+    //        for (p <- k to cols-1) {
+    //          sums(j)(j)(k)(p) = getRowSum(j,k,p)
+    //          val s = sums(j)(j)(k)(p)
+    //          //println(k + " " + p + " : " + s)
+    //          if (s <= K && s > globalMax) {
+    //            globalMax = s
+    //            //println("Global max " + globalMax)
+    //          }
+    //        }
+    //      }
+    //    }
 
 
     //println(rowSums.get(0).get.mkString(","))
     //println(rowSums.get(1).get.mkString(","))
 
     for (bRow <- 0 to rows-1) {
-      for (eRow <- bRow+1 to rows-1) {
+      for (eRow <- bRow to rows-1) {
         for (bCol <- 0 to cols-1) {
           for (eCol <- bCol to cols-1) {
             //println("Here")
-            sums(bRow)(eRow)(bCol)(eCol) = sums(bRow)(eRow-1)(bCol)(eCol) + getRowSum(eRow,bCol,eCol)
+            if (bRow == eRow) {
+              sums(bCol)(eCol) = getRowSum(eRow,bCol,eCol)
+            }else {
+              sums(bCol)(eCol) = sums(bCol)(eCol) + getRowSum(eRow,bCol,eCol)
+            }
 
-            val s = sums(bRow)(eRow)(bCol)(eCol)
+            val s = sums(bCol)(eCol)
             //println(bRow + " " + eRow + " " + bCol + " " + eCol + " => " + s + " ctxt " + sums(bRow)(eRow-1)(bCol)(eCol))
             if (s <= K && s > globalMax) {
               globalMax = s
