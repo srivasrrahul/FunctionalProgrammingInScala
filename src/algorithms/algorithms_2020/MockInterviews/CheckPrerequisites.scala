ac99c986963ba780a3ddf04b2eab2f20877a2165
srivasrrahul <srivasrrahul@gmail.com>
2020-09-03 16:17:55 +0530

    Check PreRequisites

--- src/algorithms/algorithms_2020/MockInterviews/CheckPrerequisites.scala
+++ src/algorithms/algorithms_2020/MockInterviews/CheckPrerequisites.scala
@@ -29,71 +29,62 @@ object Solution {
     }
 
 
-    var time = 0
-    def getNewTime() : Int = {
-      time = time + 1
-      time
-    }
+//    var time = 0
+//    def getNewTime() : Int = {
+//      time = time + 1
+//      time
+//    }
+//
+//    def dfs(s : Int) : (mutable.HashSet[Int],mutable.HashMap[Int,mutable.HashSet[Int]]) = {
+////      val preTimes = new mutable.HashMap[Int,Int]()
+////      val postTimes = new mutable.HashMap[Int,Int]()
+//      val visited = new mutable.HashSet[Int]()
+//      val parent = new mutable.HashMap[Int,mutable.HashSet[Int]]()
+//      def explore(u : Int) : Unit = {
+//        visited.add(u)
+//
+//        //preTimes += ((u,getNewTime()))
+//        for (v <- graph.getEdges(u)) {
+//          val defaultSet = parent.getOrElseUpdate(v,new mutable.HashSet[Int]())
+//          defaultSet.add(u)
+//          if (visited.contains(v) == false) {
+//            explore(v)
+//          }
+//        }
+//
+//        //postTimes += ((u,getNewTime()))
+//      }
+//
+//      explore(s)
+//
+//      (visited,parent)
+//      //(preTimes,postTimes)
+//    }
 
-    def dfs(s : Int) : (mutable.HashSet[Int],mutable.HashMap[Int,mutable.HashSet[Int]]) = {
-//      val preTimes = new mutable.HashMap[Int,Int]()
-//      val postTimes = new mutable.HashMap[Int,Int]()
-      val visited = new mutable.HashSet[Int]()
-      val parent = new mutable.HashMap[Int,mutable.HashSet[Int]]()
-      def explore(u : Int) : Unit = {
-        visited.add(u)
 
-        //preTimes += ((u,getNewTime()))
-        for (v <- graph.getEdges(u)) {
-          val defaultSet = parent.getOrElseUpdate(v,new mutable.HashSet[Int]())
-          defaultSet.add(u)
-          if (visited.contains(v) == false) {
-            explore(v)
-          }
-        }
-
-        //postTimes += ((u,getNewTime()))
-      }
-
-      explore(s)
-
-      (visited,parent)
-      //(preTimes,postTimes)
-    }
-
-
-    val visitedAndParents = new mutable.HashMap[Int,(mutable.HashSet[Int],mutable.HashMap[Int,mutable.HashSet[Int]])]()
-    for (s <- graph.findNodesWithNoDegrees()) {
-      val visitedParent = dfs(s)
-      visitedAndParents += ((s,visitedParent))
-    }
+//    val visitedAndParents = new mutable.HashMap[Int,(mutable.HashSet[Int],mutable.HashMap[Int,mutable.HashSet[Int]])]()
+//    for (s <- graph.findNodesWithNoDegrees()) {
+//      val visitedParent = dfs(s)
+//      visitedAndParents += ((s,visitedParent))
+//    }
 
 
     def checkDep(u : Int,v : Int) : Boolean = {
+      val q = new mutable.Queue[Int]()
       var depFound = false
-      for ((source,(visited,parents)) <- visitedAndParents if depFound == false) {
-        if (visited.contains(u) && visited.contains(v)) {
-          def findParentTillSource(current : Int,target : Int) : Boolean = {
-            if (current == source) {
-              source == target
-            }else {
-              if (target == current) {
-                true
-              }else {
-                val currentParents = parents.get(current).get
-                var found = false
-                for (currentParent <- currentParents if found == false) {
-                  found = findParentTillSource(currentParent,target)
-                }
-
-                found
-              }
+      val localVisited = new mutable.HashSet[Int]()
+      while (q.isEmpty == false && depFound == false) {
+        val top = q.dequeue()
+        localVisited.add(top)
+        for (next <- graph.getEdges(top)) {
+          if (next == v) {
+            depFound = true
+          }else {
+            if (localVisited.contains(next) == false) {
+              q.append(next)
             }
           }
-
-          depFound = findParentTillSource(v,u)
         }
-
       }
 
       depFound
