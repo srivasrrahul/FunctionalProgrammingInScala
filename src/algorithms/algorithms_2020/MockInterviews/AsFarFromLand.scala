5efb9385e0932478f1f5e3d846c89772d423aab1
srivasrrahul <srivasrrahul@gmail.com>
2020-09-04 12:02:55 +0530

    As far from Land

--- src/algorithms/algorithms_2020/MockInterviews/AsFarFromLand.scala
+++ src/algorithms/algorithms_2020/MockInterviews/AsFarFromLand.scala
@@ -1,117 +1,105 @@
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
-//case class NodeId(val x : Int,val y : Int)
-//class Graph {
-//  val nodes = new mutable.HashMap[NodeId,mutable.HashSet[NodeId]]()
-//
-//  def addNode(n1 : NodeId,n2 : NodeId) : Unit = {
-//    val s1 = nodes.getOrElseUpdate(n1,new mutable.HashSet[NodeId]())
-//    s1.add(n2)
-//
-//    val s2 = nodes.getOrElseUpdate(n2,new mutable.HashSet[NodeId]())
-//    s2.add(n1)
-//  }
-//
-//  def getNext(n1 : NodeId) : Set[NodeId] = {
-//    nodes.getOrElse(n1,new mutable.HashSet[NodeId]()).toSet
-//  }
-//}
+
 object Solution {
   def maxDistance(grid: Array[Array[Int]]): Int = {
     //run bfs from each water land and stor in TreeSet
     val rows = grid.length
     val cols = grid(0).length
 
-    val totalVertex = rows*cols
-
-    val dist = Array.ofDim[Int](totalVertex,totalVertex)
-
     def getIndex(vertexId : Int) : (Int,Int) = {
       (vertexId/cols,vertexId % cols)
     }
 
-    for (j <- 0 to totalVertex-1) {
-      for (k <- 0 to totalVertex-1) {
-        if (j != k) {
-          dist(j)(k) = Int.MaxValue
-        }else {
-          dist(j)(k) = 0
-        }
-      }
+
+    def getVertex(j : Int,k : Int) : Int = {
+      j*cols+k
     }
 
-    for (j <- 0 to rows-1) {
-      for (k <- 0 to cols-1) {
-        val current = j*cols + k
-        if (j-1>= 0) {
-          val aboveId = (j-1)*cols + k
-          dist(current)(aboveId) = 1
-        }
-
-        if (j+1 < rows) {
-          val belowId = (j+1)*cols + k
-          dist(current)(belowId) = 1
-        }
-
-        if (k-1 >= 0) {
-          val left = j*cols + (k-1)
-          dist(current)(left) = 1
-        }
-
-        if (k+1 < cols) {
-          val right = j*cols + (k+1)
-          dist(current)(right) = 1
-        }
+    def getNeigbour(vertex : Int) : List[Int] = {
+      val (j,k) = getIndex(vertex)
+      val lst = new ListBuffer[Int]
+      if (j-1 >= 0) {
+        lst.append(getVertex(j-1,k))
       }
+
+      if (j+1 < rows) {
+        lst.append(getVertex(j+1,k))
+      }
+
+      if (k-1 >= 0) {
+        lst.append(getVertex(j,k-1))
+      }
+
+      if (k+1 < cols) {
+        lst.append(getVertex(j,k+1))
+      }
+
+      lst.toList
     }
 
-    for (k <- 0 to totalVertex-1) {
-      for (i <- 0 to totalVertex-1) {
-        for (j <- 0 to totalVertex-1) {
-          //relax the edge
-          if (dist(i)(k) != Int.MaxValue && dist(k)(j) != Int.MaxValue) {
-            if (dist(i)(k) + dist(k)(j) < dist(i)(j)) {
-              dist(i)(j) = dist(i)(k) + dist(k)(j)
+
+    def bfs(vertex : Int) : Int = { //path is water
+      //min path from nearest land
+      val pathLens = new mutable.HashMap[Int,Int]()
+      val q = new mutable.Queue[Int]()
+
+      q.clear()
+      q.append(vertex)
+
+      pathLens.clear()
+
+      //val visited = new mutable.HashSet[Int]()
+      //val pathLens = new mutable.HashMap[Int,Int]()
+      pathLens += ((vertex,0))
+
+      var minPath = Int.MaxValue
+      while (q.isEmpty == false && minPath == Int.MaxValue) {
+        val top = q.dequeue()
+        //visited.add(top)
+
+        val existingPathLen = pathLens.get(top).get //has to be there
+        for (v <- getNeigbour(top)) {
+          val (j1,k1) = getIndex(v)
+          if (pathLens.contains(v) == false) { //its a land found it
+            pathLens += ((v,existingPathLen+1))
+            q.append(v)
+            if (grid(j1)(k1) == 1) {
+              //its a land
+              minPath = existingPathLen+1
             }
           }
         }
       }
+
+
+      //      for ((v,pathLen) <- pathLens) {
+      //        val (j,k) = getIndex(v)
+      //        if (grid(j)(k) == 1 && pathLen < minPath) { //land
+      //          minPath = pathLen
+      //        }
+      //      }
+      //
+      //      pathLens.clear()
+
+      minPath
     }
 
-    //Now you have all pair distances
-    var minDistances = new mutable.ArrayBuffer[Int]
+    var maxDistance = -1
     for (j <- 0 to rows-1) {
       for (k <- 0 to cols-1) {
-
-        if (grid(j)(k) == 0) { //its water
-          val vertex = j*cols + k
-          var minDistance = Int.MaxValue
-          for (otherVertx <- 0 to totalVertex-1 if otherVertx != vertex) {
-            val (j1,k1) = getIndex(otherVertx)
-            if (grid(j1)(k1) == 1) { //its land
-              val distance = dist(vertex)(otherVertx)
-              if (distance < minDistance) {
-                minDistance = distance
-              }
-            }
-          }
-
-          if (minDistance != Int.MaxValue) {
-            minDistances.append(minDistance)
+        if (grid(j)(k) == 0) {
+          //println(j + " " + k)
+          val minPath = bfs(getVertex(j,k))
+          if (minPath != Int.MaxValue && minPath > maxDistance) {
+            maxDistance = minPath
           }
         }
       }
     }
 
-
-
-    if (minDistances.isEmpty == false) {
-      minDistances.max
-
-    }else {
-      -1
-    }
-
+    maxDistance
 
 
 
