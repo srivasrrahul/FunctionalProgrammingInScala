29a40a9ff035c776226a7ab5f8ebf1bd859352b4
srivasrrahul <srivasrrahul@gmail.com>
2020-09-11 09:46:23 +0530

    Check Rectangle

--- src/algorithms/algorithms_2020/MockInterviews/CheckRectangle.scala
+++ src/algorithms/algorithms_2020/MockInterviews/CheckRectangle.scala
@@ -1,5 +1,14 @@
 object Solution {
   def isRectangleOverlap(rec1: Array[Int], rec2: Array[Int]): Boolean = {
-    !(rec1(2) <= rec2(0) || rec1(0) >= rec2(2) || rec1(3) <= rec2(1) || rec2(3) <= rec1(1))
+    var retValue = true
+    if (rec1(2) <= rec2(0) || rec2(2) <= rec1(0)) {
+      retValue = false
+    }
+
+    if (rec1(3) <= rec2(1) || rec2(3) <= rec1(1)) {
+      retValue = false
+    }
+
+    retValue
   }
 }
