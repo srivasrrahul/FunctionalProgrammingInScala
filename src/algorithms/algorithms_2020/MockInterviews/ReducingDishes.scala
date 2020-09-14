c141d3d1d6a16f6570c988523db452faf48f7d99
srivasrrahul <srivasrrahul@gmail.com>
2020-09-14 16:57:52 +0530

    Reducing Dishes

--- src/algorithms/algorithms_2020/MockInterviews/ReducingDishes.scala
+++ src/algorithms/algorithms_2020/MockInterviews/ReducingDishes.scala
@@ -1,56 +1,79 @@
 import scala.collection.mutable
 
-case class Index(val used : List[Int],val unused : List[Int])
+case class Index(val nIndex : Int,val negSelected : List[Int])
 object Solution {
   def maxSatisfaction(satisfaction: Array[Int]): Int = {
     //visited in this order
-    val cache = new mutable.HashMap[Index,Int]()
-    def itr(used : List[Int],discarded : List[Int]) : Int = {
-      if (satisfaction.length == (used.length + discarded.length)) {
-        var j = 1
-        var likes = 0
-        for (useIndex <- used.reverse) {
-          likes = likes + j*satisfaction(useIndex)
-          j = j + 1
+    val negativesArr = new mutable.ArrayBuffer[(Int,Int)] //(value,index)
+    val positiveArr = new mutable.ArrayBuffer[(Int,Int)]
+    for (j <- 0 to satisfaction.length-1) {
+      val num = satisfaction(j)
+      if (num < 0) {
+        negativesArr.append((j,num))
+      }else {
+        positiveArr.append((j,num))
+      }
+    }
+
+    positiveArr.sortInPlace()(new Ordering[(Int,Int)] {
+      override def compare(x: (Int, Int), y: (Int, Int)): Int = {
+        x._2.compareTo(y._2)
+
+      }
+    })
+    negativesArr.sortInPlace()(new Ordering[(Int,Int)] {
+      override def compare(x: (Int, Int), y: (Int, Int)): Int = {
+        x._2.compareTo(y._2)
+      }
+    })
+
+    //println(positiveArr)
+
+    val positiveSum = positiveArr.foldRight(0)((newVal,s) => s + newVal._2)
+    var basicLikes = 0
+    var st = 1
+//    println(positiveArr)
+//    println(basicLikes)
+    for (j <- 0 to positiveArr.length-1) {
+      basicLikes = basicLikes + st*positiveArr(j)._2
+      st = st + 1
+    }
+
+    //basicLikes
+    def positiveLikes(startTime : Int) : Int = {
+
+      val diff = startTime-1
+      val res = basicLikes + diff*positiveSum
+      //println(startTime + " " + res)
+      res
+    }
+    def itr(nIndex : Int,negativesSelected : List[Int]) : Int = {
+      if (nIndex < 0) {
+        var time = negativesSelected.length+1
+        var likes = positiveLikes(negativesSelected.length+1)
+//        for ((_,pValue) <- positiveArr) {
+//          likes = likes + pValue*time
+//          time = time + 1
+//        }
+
+        time = 1
+        for (nIndex <- negativesSelected) {
+          likes = likes + negativesArr(nIndex)._2*time
+          time = time+1
         }
 
         likes
       }else {
-        val index = new Index(used,discarded)
-        if (cache.contains(index)) {
-          //println("cache hit")
-          cache.get(index).get
-        }else {
-          var globalMax = Int.MinValue
-          for (p <- 0 to satisfaction.length - 1) {
-            //two choices
-            if (used.contains(p) == false && discarded.contains(p) == false) {
-              //two choices
-              //choice-1
-              if (satisfaction(p) < 0) {
-                val option1 = itr(p :: used, discarded)
-                val option2 = itr(used, p :: discarded)
-                val maxOption = math.max(option1, option2)
-                if (maxOption > globalMax) {
-                  globalMax = maxOption
-                }
-              }else {
-                val maxOption = itr(p :: used, discarded)
-                if (maxOption > globalMax) {
-                  globalMax = maxOption
-                }
-              }
-
-            }
-          }
-
-          cache += ((index,globalMax))
-          globalMax
-        }
+        //select nIndex or don't
+        val opt1 = itr(nIndex - 1, nIndex :: negativesSelected)
+        val opt2 = itr(nIndex - 1, negativesSelected)
+        //cache += ((index,math.max(opt1, opt2)))
+        math.max(opt1, opt2)
       }
+
     }
 
-    itr(List(),List())
+    itr(negativesArr.length-1,List())
   }
 
   def main(args: Array[String]): Unit = {
