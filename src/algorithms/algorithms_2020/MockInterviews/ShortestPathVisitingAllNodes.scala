5e3ef14937f1db38aa64d105ec8b7ca6e62de47f
srivasrrahul <srivasrrahul@gmail.com>
2020-09-24 12:20:44 +0530

    Shortest Path Visiting All Nodes

--- src/algorithms/algorithms_2020/MockInterviews/ShortestPathVisitingAllNodes.scala
+++ src/algorithms/algorithms_2020/MockInterviews/ShortestPathVisitingAllNodes.scala
@@ -1,59 +1,80 @@
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
-case class Edge(val x : Int,val y : Int)
+case class Index(val s : Set[Int],val u : Int)
 object Solution {
   def shortestPathLength(graph: Array[Array[Int]]): Int = {
-    val edgeDB = new mutable.HashMap[Edge,Int]()
-    var id = 0
+    val allPairShortestPaths = Array.ofDim[Int](graph.length,graph.length)
     for (j <- 0 to graph.length-1) {
-      for (k <- graph(j)) {
-        val edge = new Edge(j,k)
-        edgeDB += ((edge,id))
-        id = id + 1
+      for (k <- 0 to graph.length-1) {
+        allPairShortestPaths(j)(k) = Int.MaxValue
       }
     }
 
-    //println(edgeDB)
+    for (j <- 0 to graph.length-1) {
+      allPairShortestPaths(j)(j) = 0
+    }
 
-    def sp(u : Int,visited : Set[Int],visitedEdgeIds : Set[Int]) : Option[Int] = {
-      //println(u + " " + visited + " " + visitedEdgeIds)
-      if (visited.size == graph.length) {
-        Some(visitedEdgeIds.size)
-      }else {
-        var globalMin = Int.MaxValue
-        for (v <- graph(u)) {
-          val edge = new Edge(u,v)
-          val edgeId = edgeDB.get(edge).get
-          if (visitedEdgeIds.contains(edgeId) == false) {
-            val minPath = sp(v,visited.+(u),visitedEdgeIds.+(edgeId))
-            if (minPath.isDefined && minPath.get < globalMin) {
-              globalMin = minPath.get
+    for (u <- 0 to graph.length-1) {
+      for (v <- graph(u)) {
+        allPairShortestPaths(u)(v) = 1
+      }
+    }
+
+    for (k <- 0 to graph.length-1) {
+      for (i <- 0 to graph.length-1) {
+        for (j <- 0 to graph.length-1) {
+          val p1 = allPairShortestPaths(i)(k)
+          val p2 = allPairShortestPaths(k)(j)
+          if (p1 != Int.MaxValue && p2 != Int.MaxValue) {
+            val p = p1 + p2
+            if (p < allPairShortestPaths(i)(j)) {
+              allPairShortestPaths(i)(j) = p
             }
           }
         }
+      }
+    }
 
-        if (globalMin == Int.MaxValue) {
-          None
+
+
+    val cache = new mutable.HashMap[Index,Int]()
+    def sp(notVisted : Set[Int],current : Int) : Int = {
+      if (notVisted.size == 0) {
+        0
+      }else {
+        if (notVisted.size == 1) {
+          allPairShortestPaths(notVisted.head)(current)
         }else {
-          Some(globalMin)
+          val index = new Index(notVisted,current)
+          if (cache.contains(index)) {
+            cache.get(index).get
+          }else {
+            var minCost = Int.MaxValue
+            for (u <- notVisted) {
+              val cost = allPairShortestPaths(u)(current) + sp(notVisted.-(u), u)
+              if (cost < minCost) {
+                minCost = cost
+              }
+            }
+
+            cache += ((index,minCost))
+            minCost
+          }
         }
       }
     }
 
-    var globalMin = Int.MaxValue
-    for (j <- 0 to graph.length-1) {
-      //println("For j " + j)
-      val minPath = sp(j,Set(),Set())
-      if (minPath.isDefined && minPath.get < globalMin) {
-        globalMin = minPath.get
+    val S = Range(0,graph.length).toSet
+    var minCost = Int.MaxValue
+    for (j <- graph.length-1 to 0 by -1) {
+      val cost = sp(S.-(j),j)
+      if (cost < minCost) {
+        minCost = cost
       }
     }
 
-    if (globalMin > 0) {
-      globalMin-1
-    }else {
-      globalMin
-    }
+    minCost
 
   }
 
