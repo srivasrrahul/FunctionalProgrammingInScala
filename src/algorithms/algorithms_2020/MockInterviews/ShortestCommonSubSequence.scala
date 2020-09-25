02e5d6b190f3078617e05c1f09de2dcb32978b52
srivasrrahul <srivasrrahul@gmail.com>
2020-09-25 10:07:36 +0530

    Shortest Common SubSequence

--- src/algorithms/algorithms_2020/MockInterviews/ShortestCommonSubSequence.scala
+++ src/algorithms/algorithms_2020/MockInterviews/ShortestCommonSubSequence.scala
@@ -3,89 +3,87 @@ import scala.collection.mutable
 case class Index(val x : Int,val y : Int)
 object Solution {
   def shortestCommonSupersequence(str1: String, str2: String): String = {
-    val cache = new mutable.HashMap[Index,String]()
-    def itr(j : Int,k : Int) : String = {
-      if (j == 0 || k == 0) {
-        //Either of them is 0
-        //find the substr
-        //println(j + " " + k)
-        if (j == 0 && k == 0) {
-          if (str1(j) == str2(k)) {
-            str1(j).toString
-          }else {
-            val ret = str1(j).toString ++ str2(k).toString
-            //println("ret " + ret)
-            ret
-          }
-        }else {
-          var count = 0
-          if (j == 0) {
-            var found = false
-            for (x <- 0 to k) {
-              if (str2(x) == str1(j)) {
-                found = true
-              }
-            }
-
-            if (found) {
-              str2.substring(0,k+1)
-            }else {
-              str2.substring(0,k+1) ++ str1(j).toString
-            }
-          }else {
-            var found = false
-            for (x <- 0 to j) {
-              if (str1(x) == str2(k)) {
-                found = true
-              }
-            }
-
-            if (found) {
-              str1.substring(0,j+1)
-            }else {
-              str1.substring(0,j+1) ++ str2(k).toString
-            }
-          }
-        }
+    val arr = Array.ofDim[Int](str1.length,str2.length)
+    var isSubStr  = false
+    for (j <- 0 to str1.length-1) {
+      if (str2(0) == str1(j)) {
+        arr(j)(0) = j+1
+        isSubStr = true
       }else {
-        val index = new Index(j,k)
-        if (cache.contains(index)) {
-          cache.get(index).get
+        if (isSubStr) {
+          arr(j)(0) = j+1
         }else {
-          if (str1(j) == str2(k)) {
-            val pending = itr(j - 1, k - 1) //++ str1(j).toString
-            val stringBuilder = new StringBuilder
-            stringBuilder.appendAll(pending)
-            stringBuilder.append(str1(j))
-            val retValue = stringBuilder.toString()
-            cache += ((index,retValue))
-            retValue
-          } else {
-            val opt1 = itr(j - 1, k)
-            val opt2 = itr(j, k - 1)
-            if (opt1.length <= opt2.length) {
-
-              val stringBuilder = new StringBuilder
-              stringBuilder.appendAll(opt1)
-
-              //val retValue = opt1 ++ str1(j).toString
-              val retValue = stringBuilder.append(str1(j)).toString()
-              cache += ((index,retValue))
-              retValue
-            } else {
-              val stringBuilder = new StringBuilder
-              stringBuilder.appendAll(opt2)
-              stringBuilder.append(str2(k))
-              val retValue = stringBuilder.toString()
-              cache += ((index,retValue))
-              retValue
-            }
-          }
+          arr(j)(0) = j+2
         }
       }
     }
 
-    itr(str1.length-1,str2.length-1)
+    isSubStr = false
+    for (k <- 0 to str2.length-1) {
+      if (str1(0) == str2(k)) {
+        arr(0)(k) = k+1
+        isSubStr = true
+      }else {
+        if (isSubStr) {
+          arr(0)(k) = k+1
+        }else {
+          arr(0)(k) = k +2
+        }
+      }
+    }
+
+    for (j <- 1 to str1.length-1) {
+      for (k <- 1 to str2.length-1) {
+        if (str1(j) == str2(k)) {
+          arr(j)(k) = arr(j-1)(k-1) + 1
+        }else {
+          arr(j)(k) = math.min(arr(j-1)(k),arr(j)(k-1)) + 1
+        }
+      }
+    }
+
+    val strBuilder = new StringBuilder
+    var j = str1.length-1
+    var k = str2.length-1
+
+    while (j != 0 && k != 0) {
+      if (str1(j) != str2(k)) {
+        //Move in lower direction
+        if (arr(j-1)(k) < arr(j)(k-1)) {
+          strBuilder.append(str1(j))
+          j = j -1
+        }else {
+          strBuilder.append(str2(k))
+          k = k -1
+        }
+      }else {
+        strBuilder.append(str2(k))
+        j = j - 1
+        k = k - 1
+      }
+    }
+
+    if (j == 0) {
+      for (p <- k to 0 by -1) {
+        strBuilder.append(str2(p))
+      }
+
+      if (arr(0)(k) == k+2) {
+        strBuilder.append(str1(0))
+      }
+
+    }else {
+      for (p <- j to 0 by -1) {
+        strBuilder.append(str1(p))
+      }
+
+      if (arr(j)(0) == j+2) {
+        strBuilder.append(str2(0))
+      }
+    }
+
+    strBuilder.reverse.toString()
+    //"abc"
   }
 
   def main(args: Array[String]): Unit = {
