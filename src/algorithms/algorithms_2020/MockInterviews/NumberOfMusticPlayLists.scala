ade0a1397517d67619f01af23dda849620551936
srivasrrahul <srivasrrahul@gmail.com>
2020-09-27 11:25:09 +0530

    Number of Music PL

--- src/algorithms/algorithms_2020/MockInterviews/NumberOfMusticPlayLists.scala
+++ src/algorithms/algorithms_2020/MockInterviews/NumberOfMusticPlayLists.scala
@@ -5,46 +5,48 @@ case class Index(val l : Int,val pl :  Set[Int] )
 object Solution {
   def numMusicPlaylists(N: Int, L: Int, K: Int): Int = {
 
-    def itr(l : Int,n : Int) : List[List[Int]] = {
-      //println(l + "  " + n)
-      if (l == 1) {
-        List(List(n))
-      }else {
-        var count = 0
-        val lstBuffer = new ListBuffer[List[Int]]
-        for (j <- 1 to N) {
-          if (j != n) {
-            val lsts = itr(l-1,j)
-            //println(l + " " + n + " " + (l-1) + " " + j + " " + lsts)
+    var arr = new mutable.ArrayBuffer[List[List[Int]]]()
+    for (j <- 0 to N-1) {
+      arr.append(List(List(j)))
+    }
 
+    for (l <- 2 to L) {
+      var newArr = new mutable.ArrayBuffer[List[List[Int]]]()
+      for (j <- 0 to N-1) {
+        val lstBuffer = new ListBuffer[List[Int]]
+        for (k <- 0 to N-1) {
+          if (j != k) {
+            val lsts = arr(k)
             for (lst <- lsts) {
               var k = 1
               var canAdded = true
               for (ls <- lst if k <= K) {
-                if (ls == n) {
+                if (ls == j) {
                   canAdded = false
                 }
+
                 k = k + 1
               }
 
               if (canAdded) {
-                lstBuffer.append(n :: lst)
+                lstBuffer.append(j :: lst)
               }
-
             }
-
-
           }
         }
 
-        lstBuffer.toList
+        newArr.append(lstBuffer.toList)
       }
+
+      println(newArr)
+
+      arr.clear()
+      arr = newArr
     }
 
+
     var count = 0
-    for (j <- 1 to N) {
-      val lsts  = itr(L,j)
-      println(lsts)
+    for (lsts <- arr) {
       for (lst <- lsts) {
         if (lst.toSet.size == N) {
           count = count + 1
@@ -57,6 +59,6 @@ object Solution {
 
   def main(args: Array[String]): Unit = {
 
-    println(numMusicPlaylists(3,3,1))
+    println(numMusicPlaylists(3,3,2))
   }
 }
