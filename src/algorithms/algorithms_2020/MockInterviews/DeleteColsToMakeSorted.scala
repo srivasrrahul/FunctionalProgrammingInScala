56105806e86606a30afeb58488751192d3592753
srivasrrahul <srivasrrahul@gmail.com>
2020-09-28 21:29:36 +0530

    Delete Cols to Make sorted

--- src/algorithms/algorithms_2020/MockInterviews/DeleteColsToMakeSorted.scala
+++ src/algorithms/algorithms_2020/MockInterviews/DeleteColsToMakeSorted.scala
@@ -4,66 +4,39 @@ import scala.collection.immutable
 case class Index(val s : Set[Int],val t : immutable.TreeSet[Int])
 object Solution {
   def minDeletionSize(A: Array[String]): Int = {
-    def checkSorted(j : Int,pendingCols : immutable.TreeSet[Int]) : Boolean = {
-      if (pendingCols.size == 1) {
-        true
-      }else {
-        var sorted = true
-        var prev = pendingCols.head
-        for (t <- pendingCols.tail if sorted == true) {
-          if (A(j)(t) < A(j)(prev)) {
-            sorted = false
-          }else {
-            prev = t
+    val rows = A.length
+    val cols = A(0).length
+    val sorted = new Array[Int](cols)
+
+    sorted(0) = 1
+    for (j <- 1 to cols-1) {
+      var allowedMaxSorted = new mutable.ArrayBuffer[Int]
+      for (k <- j-1 to 0 by -1) {
+        var isLower = true
+        for (p <- 0 to rows-1) {
+          if (A(p)(k) > A(p)(j)) {
+            isLower = false
           }
         }
 
-        sorted
-      }
-    }
-
-    def checkAllSorted(s : Set[Int], p : immutable.TreeSet[Int]) : Boolean = {
-      var allSorted = true
-      for (j <- s if allSorted == true) {
-        if (checkSorted(j,p) == false) {
-          allSorted = false
+        if (isLower) {
+          val maxSorted = 1+sorted(k)
+          allowedMaxSorted.append(maxSorted)
         }
       }
 
-      allSorted
-    }
-
-    val cache = new mutable.HashMap[Index,Int]()
-    def itr(unsortedRows : Set[Int],pendingCols : immutable.TreeSet[Int]) : Int = {
-      if (unsortedRows.size == 0 || pendingCols.size == 1 || checkAllSorted(unsortedRows,pendingCols)) {
-        0
+      if (allowedMaxSorted.isEmpty) {
+        sorted(j) = 1
       }else {
-        val index = new Index(unsortedRows,pendingCols)
-        if (cache.contains(index)) {
-          cache.get(index).get
-        }else {
-          var globalCount = Int.MaxValue
-          for (j <- pendingCols) {
-            val colLst = pendingCols.-(j)
-            val sortedlst = new mutable.HashSet[Int]()
-            for (k <- unsortedRows) {
-              if (checkSorted(k, colLst)) {
-                sortedlst.add(k)
-              }
-            }
-
-            val count = 1 + itr(unsortedRows.diff(sortedlst.toSet), colLst)
-            if (count < globalCount) {
-              globalCount = count
-            }
-          }
-
-          cache += ((index,globalCount))
-          globalCount
-        }
+        sorted(j) = allowedMaxSorted.max
       }
     }
 
-    itr(Range(0,A.length).toSet,new immutable.TreeSet[Int].++(Range(0,A(0).length).toSet))
+    //println(sorted.mkString(","))
+    cols - sorted.max
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(minDeletionSize(Array("pmtra","qbrns")))
   }
 }
