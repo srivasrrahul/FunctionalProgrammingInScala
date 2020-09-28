08df0bb30f79e04066cc9d6df4ee67ef5c62f423
srivasrrahul <srivasrrahul@gmail.com>
2020-09-28 11:00:36 +0530

    Stone Game 3

--- src/algorithms/algorithms_2020/MockInterviews/StoneGame3.scala
+++ src/algorithms/algorithms_2020/MockInterviews/StoneGame3.scala
@@ -2,99 +2,84 @@ import scala.collection.mutable
 
 case class Index(val j : Int,val a : Int,val b : Int)
 object Solution {
-  def stoneGameIII(stoneValue: Array[Int]): String = {
+  def stoneGameIII(A: Array[Int]): String = {
 
-    def getValue(j : Int) : Int = {
-      if (j >= stoneValue.length) {
-        0
+
+    val cache = new mutable.HashMap[Int,(Int,Int)]()
+    def score(j : Int) : (Int,Int) = {
+      //score of current and next Pointer
+      if (j >= A.length) {
+        (0,A.length)
       }else {
-        stoneValue(j)
-      }
-    }
-
-    val cache = new mutable.HashMap[Index,Int]()
-    def itr(j : Int,playerId : Int, aScore : Int,bScore : Int) : Int = {
-      if (j >= stoneValue.length) {
-        if (playerId == 0) {
-          if (aScore > bScore) {
-            0
-          } else {
-            if (aScore < bScore) {
-              2
-            } else {
-              1
+        if (j == A.length-1) {
+          (A(j),A.length)
+        }else {
+          if (cache.contains(j)) {
+            cache.get(j).get
+          }else {
+            val arr = new mutable.ArrayBuffer[List[Int]]()
+            var op1 = A(j)
+            val (n1, p1) = score(j + 1)
+            if (p1 < A.length) {
+              op1 = op1 + score(p1)._1
             }
-          }
-        } else {
-          if (bScore > aScore) {
-            0
-          } else {
-            if (bScore < aScore) {
-              2
-            } else {
-              1
-            }
-          }
-        }
-      } else {
-        val index = new Index(j, aScore, bScore)
-        if (cache.contains(index)) {
-          cache.get(index).get
-        } else {
-          var res = 0
-          if (playerId == 0) {
-            val res1 = itr(j + 1, 1, aScore + getValue(j), bScore)
-            val res2 = itr(j + 2, 1, aScore + getValue(j) + getValue(j + 1), bScore)
-            val res3 = itr(j + 3, 1, aScore + getValue(j) + getValue(j + 1) + getValue(j + 2), bScore)
 
+            arr.append(List(op1, n1, j + 1))
 
-            if (res1 == 2 || res2 == 2 || res3 == 2) {
-              res = 0 //won as other side lost from here
-            } else {
-              if (res1 == 1 || res2 == 1 || res3 == 1) {
-                res = 1 //draw
-              } else {
-                res = 2 //lost
+            var o2 = 0
+            if (j + 1 < A.length) {
+              val (n2, p2) = score(j + 2)
+              o2 = A(j) + A(j + 1)
+              if (p2 < A.length) {
+                o2 = o2 + score(p2)._1
               }
-            }
-          } else {
-            val res1 = itr(j + 1, 0, aScore, bScore + getValue(j))
-            val res2 = itr(j + 2, 0, aScore, bScore + getValue(j) + getValue(j + 1))
-            val res3 = itr(j + 3, 0, aScore, bScore + getValue(j) + getValue(j + 1) + getValue(j + 2))
 
-            if (res1 == 2 || res2 == 2 || res3 == 2) {
-              res = 0 //won as 1 lost
-            } else {
-              if (res1 == 1 || res2 == 1 || res3 == 1) {
-                res = 1 //draw
-              } else {
-                res = 2 //lost
+              arr.append(List(o2, n2, j + 2))
+            }
+
+            var o3 = 0
+            if (j + 2 < A.length) {
+              val (n3, p3) = score(j + 3)
+              o3 = A(j) + A(j + 1) + A(j + 2)
+              if (p3 < A.length) {
+                o3 = o3 + score(p3)._1
               }
+
+              arr.append(List(o3, n3, j + 3))
             }
 
+            arr.sortInPlace()(new Ordering[List[Int]] {
+              override def compare(x: List[Int], y: List[Int]): Int = {
+                val d1 = x.head - x.tail.head
+                val d2 = y.head - y.tail.head
+                d1.compareTo(d2)
+              }
+            })
 
+            val maxSum = arr.last.head
+            val next = arr.last.tail.tail.head
+            cache += ((j,((maxSum,next))))
+            (maxSum, next)
           }
-
-          cache += ((index,res))
-          res
         }
       }
     }
 
-
-    val res = itr(0,0,0,0)
-    if (res == 0) {
+    val (aScore,p1) = score(0)
+    val bScore = A.sum - aScore
+    if (aScore > bScore) {
       "Alice"
     }else {
-      if (res == 1) {
-        "Tie"
-      }else {
+      if (bScore > aScore) {
         "Bob"
+      }else {
+        "Tie"
       }
     }
+
   }
 
   def main(args: Array[String]): Unit = {
-    println(stoneGameIII(Array(1,2,3,7)))
+    println(stoneGameIII(Array(-1,-2,-3)))
   }
 }
