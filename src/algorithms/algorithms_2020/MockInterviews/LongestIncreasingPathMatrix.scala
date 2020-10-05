d5caf56a25a2c4a3a1252339202396af43f93d22
srivasrrahul <srivasrrahul@gmail.com>
2020-10-05 17:08:11 +0530

    Longest Increase Path Matrix

---
+++ src/algorithms/algorithms_2020/MockInterviews/LongestIncreasingPathMatrix.scala
@@ -0,0 +1,79 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+case class Point(val x : Int,val y : Int)
+case class Index(val p : Point,val s : Set[Point])
+object Solution {
+  def longestIncreasingPath(matrix: Array[Array[Int]]): Int = {
+    if (matrix.isEmpty) {
+      0
+    }else {
+      val rows = matrix.length
+      val cols = matrix(0).length
+
+      def next(point: Point): List[Point] = {
+        val px = point.x
+        val py = point.y
+
+        val retValue = new ListBuffer[Point]
+        if (px + 1 < rows) {
+          retValue.append(new Point(px + 1, py))
+        }
+
+        if (px - 1 >= 0) {
+          retValue.append(new Point(px - 1, py))
+        }
+
+        if (py + 1 < cols) {
+          retValue.append(new Point(px, py + 1))
+        }
+
+        if (py - 1 >= 0) {
+          retValue.append(new Point(px, py - 1))
+        }
+
+        retValue.toList
+      }
+
+      def getVal(point: Point): Int = {
+        matrix(point.x)(point.y)
+      }
+
+      val cache = new mutable.HashMap[Index, Int]()
+
+      def lis(point: Point, path: Set[Point]): Int = {
+        val index = new Index(point, path)
+        if (cache.contains(index)) {
+          cache.get(index).get
+        } else {
+          val nextPoints = next(point)
+          val newPath = path.+(point)
+          var maxPathLen = newPath.size
+          for (nextPoint <- nextPoints) {
+            if (path.contains(nextPoint) == false && getVal(nextPoint) > getVal(point)) {
+              val s = lis(nextPoint, newPath)
+              if (s > maxPathLen) {
+                maxPathLen = s
+              }
+            }
+          }
+
+          cache += ((index, maxPathLen))
+          maxPathLen
+        }
+      }
+
+      var maxPathLen = 0
+      for (j <- 0 to rows - 1) {
+        for (k <- 0 to cols - 1) {
+          val s = lis(new Point(j, k), Set())
+          if (s > maxPathLen) {
+            maxPathLen = s
+          }
+        }
+      }
+
+      maxPathLen
+    }
+  }
+}
