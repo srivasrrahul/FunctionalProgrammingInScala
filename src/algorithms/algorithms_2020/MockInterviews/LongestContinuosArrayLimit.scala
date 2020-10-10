1bdc977f25ed109dc4189019246bb335d7d90fe6
srivasrrahul <srivasrrahul@gmail.com>
2020-10-10 14:01:19 +0530

    Longest Continuos Array

--- src/algorithms/algorithms_2020/MockInterviews/LongestContinuosArrayLimit.scala
+++ src/algorithms/algorithms_2020/MockInterviews/LongestContinuosArrayLimit.scala
@@ -1,30 +1,54 @@
+import scala.collection.mutable
+
 object Solution {
   def longestSubarray(nums: Array[Int], limit: Int): Int = {
+    val slidingWindow = new mutable.TreeMap[Int,(Int,Int)]() //count and last seen index
+    slidingWindow += ((nums(0), (1,0)))
+    var maxCount = 1
+    var count = 1
 
-    var lowerLimit = 0
-    var maxArrLen = 1
     for (j <- 1 to nums.length-1) {
-      var maxAbsDiff = 1
-      var shudGoFurther = true
-      for (k <- j-1 to lowerLimit by -1 if shudGoFurther == true) {
+      if (slidingWindow.contains(nums(j))) {
+        val (defCount,defIndex) = slidingWindow.get(nums(j)).get
+        slidingWindow += ((nums(j),(defCount+1,j)))
+      }else {
+        val min = slidingWindow.head._1
+        val max = slidingWindow.last._1
+        if (nums(j) >= min && nums(j) <= max) {
+          slidingWindow += ((nums(j),(1,j)))
+        }else {
+          var maxIndex = -1
+          for ((key,(_,lastIndex)) <- slidingWindow) {
+            if (math.abs(nums(j)-key) > limit) {
+              maxIndex = math.max(maxIndex,lastIndex)
+            }
+          }
 
-        val diff = math.abs(nums(j) - nums(k))
-        if (diff <= limit) {
-          maxAbsDiff = maxAbsDiff + 1
-        } else {
-          shudGoFurther = false
-          lowerLimit = k + 1
+          val offendingKeys = new mutable.HashSet[Int]()
+          for ((key,(_,lastIndex)) <- slidingWindow) {
+            if (lastIndex <= maxIndex) {
+              offendingKeys.add(key)
+            }
+          }
+
+          for (offendingKey <- offendingKeys) {
+            slidingWindow.remove(offendingKey)
+          }
+
+          slidingWindow += ((nums(j),(1,j)))
+
         }
 
       }
 
-      if (maxAbsDiff > maxArrLen)  {
-        maxArrLen = maxAbsDiff
+      var localCount = 0
+      for ((_,(count,_)) <- slidingWindow) {
+        localCount = localCount + count
       }
 
-      //println("for j " + j + " " + lowerLimit + " " + maxAbsDiff)
+      maxCount = math.max(localCount,maxCount)
     }
 
-    maxArrLen
+    maxCount
   }
 }
