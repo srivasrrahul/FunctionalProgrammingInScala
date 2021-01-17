3ec4551c1c0c7d8eeb5809d829983f723f5dddf7
srivasrrahul <srivasrrahul@gmail.com>
2021-01-17 11:55:23 +0530

    Conform Array

--- src/algorithms/algorithms_2020/MockInterviews/ConformArray.scala
+++ src/algorithms/algorithms_2020/MockInterviews/ConformArray.scala
@@ -1,32 +1,43 @@
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object Solution {
   def canFormArray(arr: Array[Int], pieces: Array[Array[Int]]): Boolean = {
     val target = arr.toList
-    def itr(j : Int,lst : List[List[Int]]) : Boolean = {
-      //println(j)
-      if (j >= pieces.length) {
-        val lstBuffer = new ListBuffer[Int]
-        for (l <- lst) {
-          lstBuffer.appendAll(l)
-        }
 
-        lstBuffer.toList == target
-
-      }else {
-        var found = false
-        for (k <- 0 to lst.length if found == false) {
-          val (pre,post) = lst.splitAt(k)
-
-          if (itr(j+1,pre ++ List(pieces(j).toList) ++ post)) {
-            found = true
-          }
-        }
-
-        found
+    val chunkMap = new mutable.HashMap[Int,Int]()
+    for (j <- 0 to pieces.length-1) {
+      for (p <- pieces(j)) {
+        chunkMap += ((p,j))
       }
     }
 
-    itr(0,List())
+    //println(chunkMap)
+    var possible = true
+    val newArr = new ListBuffer[Int]
+    //    for (j <- 0 to arr.length-1 if possible == true) {
+    var j = 0
+    while (j < arr.length && possible == true) {
+      val x = arr(j)
+      //println(newArr.toList + " ; " + chunkMap + " ; " + x)
+
+      if (chunkMap.contains(x)) {
+        val piece = pieces(chunkMap.get(x).get)
+        for (y <- piece) {
+          newArr.append(y)
+          chunkMap.remove(y)
+          j = j + 1
+        }
+      }else {
+        possible = false
+      }
+    }
+
+    //println(newArr.toList)
+    if (possible) {
+      newArr.toList == target
+    }else {
+      false
+    }
   }
 }
